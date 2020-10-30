fd68b55ea7d1acf9ed87fecc747a698b2e06833e
chinapandaman <b.j.li@foxmail.com>
2020-10-30 16:16:47 -0500

    PPF-16: functionality and test

--- tests/functional/test_operators.py
+++ tests/functional/test_operators.py
@@ -57,3 +57,34 @@ def test_addition_operator_3_times(
         expected = f.read()
 
         assert result.stream[:comparing_size] == expected[:comparing_size]
+
+
+def test_addition_assignment_operator_3_times(
+    template_stream, image_stream, pdf_samples, comparing_size
+):
+    with open(os.path.join(pdf_samples, "sample_added_3_copies.pdf"), "rb+") as f:
+        result = PyPDFForm()
+
+        for i in range(3):
+            obj = (
+                PyPDFForm()
+                .fill(
+                    template_stream,
+                    {
+                        "test": "{}_test_1".format(i),
+                        "check": True,
+                        "test_2": "{}_test_2".format(i),
+                        "check_2": False,
+                        "test_3": "{}_test_3".format(i),
+                        "check_3": True,
+                    },
+                    simple_mode=True,
+                )
+                .draw_image(i + 1, image_stream, 100, 100, 400, 225, 0)
+            )
+
+            result += obj
+
+        expected = f.read()
+
+        assert result.stream[:comparing_size] == expected[:comparing_size]
