485b89c506e8af98cca1a2fdae3c110eefa8b489
chinapandaman <b.j.li@foxmail.com>
2020-10-29 20:56:36 -0500

    PPF-3: test fill with font 20

--- tests/functional/test_fill_pdf.py
+++ tests/functional/test_fill_pdf.py
@@ -40,3 +40,25 @@ def test_fill_simple_mode(template_stream, pdf_samples, comparing_size):
 
         assert len(obj.stream) == len(expected)
         assert obj.stream[:comparing_size] == expected[:comparing_size]
+
+
+def test_fill_font_20(template_stream, pdf_samples, comparing_size):
+    with open(os.path.join(pdf_samples, "sample_filled_font_20.pdf"), "rb+") as f:
+        obj = PyPDFForm().fill(
+            template_stream,
+            {
+                "test": "test_1",
+                "check": True,
+                "test_2": "test_2",
+                "check_2": False,
+                "test_3": "test_3",
+                "check_3": True,
+            },
+            simple_mode=False,
+            font_size=20,
+        )
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream[:comparing_size] == expected[:comparing_size]
