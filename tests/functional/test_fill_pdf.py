08efc423905c1f83e4192a8352cd54f2fbe72463
chinapandaman <b.j.li@foxmail.com>
2020-10-29 20:37:46 -0500

    PPF-3: test fill pdf simple mode

--- tests/functional/test_fill_pdf.py
+++ tests/functional/test_fill_pdf.py
@@ -1,5 +1,42 @@
 # -*- coding: utf-8 -*-
 
+import os
 
-def test_example():
-    assert True
+import pytest
+from PyPDFForm import PyPDFForm
+
+
+@pytest.fixture
+def pdf_samples():
+    return os.path.join(os.path.dirname(__file__), "..", "..", "pdf_samples")
+
+
+@pytest.fixture
+def template_stream(pdf_samples):
+    with open(os.path.join(pdf_samples, "sample_template.pdf"), "rb+") as f:
+        return f.read()
+
+
+@pytest.fixture
+def comparing_size():
+    return 32767
+
+
+def test_fill_simple_mode(template_stream, pdf_samples, comparing_size):
+    with open(os.path.join(pdf_samples, "sample_filled_simple_mode.pdf"), "rb+") as f:
+        obj = PyPDFForm().fill(
+            template_stream,
+            {
+                "test": "test_1",
+                "check": True,
+                "test_2": "test_2",
+                "check_2": False,
+                "test_3": "test_3",
+                "check_3": True,
+            },
+        )
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream[:comparing_size] == expected[:comparing_size]
