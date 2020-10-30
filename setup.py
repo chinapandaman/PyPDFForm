7d07ecee12199fdbe0c8954d68eea99c5d9e4bfb
chinapandaman <b.j.li@foxmail.com>
2020-10-29 22:01:32 -0500

    PPF-9: setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyPDFForm-chinapandaman",
+    version="0.0.1",
+    author="Jinge Li",
+    description="python library for PDF forms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chinapandaman/PyPDFForm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
