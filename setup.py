78fd3f3f39b0a727db6f408aa2d80bd10aa247fa
chinapandaman <benjaminli801@gmail.com>
2020-11-28 16:16:09 -0600

    PPF-79: bump for release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("requirements.txt", "r") as requirements:
 
 setuptools.setup(
     name="PyPDFForm",
-    version="0.1.4",
+    version="0.1.5",
     description="python library for PDF forms",
     long_description=long_description,
     long_description_content_type="text/markdown",
