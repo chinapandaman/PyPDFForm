f12d6e3fe4a4e42753f776fac7644d7143799072
chinapandaman <b.j.li@foxmail.com>
2020-12-05 20:16:57 -0600

    PPF-91: constants type hint

--- PyPDFForm/middleware/constants.py
+++ PyPDFForm/middleware/constants.py
@@ -3,37 +3,37 @@
 
 class Template(object):
     @property
-    def annotation_key(self):
+    def annotation_key(self) -> str:
         """Used for parsing PDF form with pdfrw."""
 
         return "/Annots"
 
     @property
-    def annotation_field_key(self):
+    def annotation_field_key(self) -> str:
         """Used for parsing PDF form with pdfrw."""
         
         return "/T"
 
     @property
-    def annotation_rectangle_key(self):
+    def annotation_rectangle_key(self) -> str:
         """Used for parsing PDF form with pdfrw."""
         
         return "/Rect"
 
     @property
-    def subtype_key(self):
+    def subtype_key(self) -> str:
         """Used for parsing PDF form with pdfrw."""
         
         return "/Subtype"
 
     @property
-    def widget_subtype_key(self):
+    def widget_subtype_key(self) -> str:
         """Used for parsing PDF form with pdfrw."""
         
         return "/Widget"
 
     @property
-    def element_type_key(self):
+    def element_type_key(self) -> str:
         """Used for parsing PDF form with pdfrw."""
 
         return "/FT"
