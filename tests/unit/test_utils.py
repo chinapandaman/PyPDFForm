07dd10efc24a97ff66da9ca6ae876c81002f3eae
chinapandaman <b.j.li@foxmail.com>
2020-12-06 17:49:05 -0600

    PPF-101: test bool to checkboxes

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+import pdfrw
+from copy import deepcopy
+
+from PyPDFForm.core.utils import Utils
+
+
+@pytest.fixture
+def pdf_samples():
+    return os.path.join(os.path.dirname(__file__), "..", "..", "pdf_samples")
+
+
+@pytest.fixture
+def template_stream(pdf_samples):
+    with open(os.path.join(pdf_samples, "sample_template.pdf"), "rb+") as f:
+        return f.read()
+
+
+@pytest.fixture
+def data_dict():
+    return {
+        "test": "test_1",
+        "check": True,
+        "test_2": "test_2",
+        "check_2": False,
+        "test_3": "test_3",
+        "check_3": True,
+    }
+
+
+def test_bool_to_checkboxes(data_dict):
+    result = deepcopy(data_dict)
+
+    for k, v in Utils().bool_to_checkboxes(result).items():
+        if isinstance(data_dict[k], bool):
+            assert v == (pdfrw.PdfName.Yes if data_dict[k] else pdfrw.PdfName.Off)
