500861c1ec9cc39026b18b3635740fa893517ef3
chinapandaman <b.j.li@foxmail.com>
2020-12-05 20:58:21 -0600

    PPF-91: better docstring

--- PyPDFForm/middleware/constants.py
+++ PyPDFForm/middleware/constants.py
@@ -4,36 +4,36 @@
 class Template(object):
     @property
     def annotation_key(self) -> str:
-        """Used for parsing PDF form with pdfrw."""
+        """Used for parsing PDF form via pdfrw."""
 
         return "/Annots"
 
     @property
     def annotation_field_key(self) -> str:
-        """Used for parsing PDF form with pdfrw."""
+        """Used for parsing PDF form via pdfrw."""
 
         return "/T"
 
     @property
     def annotation_rectangle_key(self) -> str:
-        """Used for parsing PDF form with pdfrw."""
+        """Used for parsing PDF form via pdfrw."""
 
         return "/Rect"
 
     @property
     def subtype_key(self) -> str:
-        """Used for parsing PDF form with pdfrw."""
+        """Used for parsing PDF form via pdfrw."""
 
         return "/Subtype"
 
     @property
     def widget_subtype_key(self) -> str:
-        """Used for parsing PDF form with pdfrw."""
+        """Used for parsing PDF form via pdfrw."""
 
         return "/Widget"
 
     @property
     def element_type_key(self) -> str:
-        """Used for parsing PDF form with pdfrw."""
+        """Used for parsing PDF form via pdfrw."""
 
         return "/FT"
