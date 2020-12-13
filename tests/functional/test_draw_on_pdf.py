bfa1722509e7e0bfe08bf038458f02d244da2c38
chinapandaman <b.j.li@foxmail.com>
2020-12-12 20:49:34 -0600

    PPF-111: test draw image on one page

---
+++ tests/functional/test_draw_on_pdf.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+from PyPDFForm.middleware.wrapper import PyPDFForm
+
+
+@pytest.fixture
+def pdf_samples():
+    return os.path.join(os.path.dirname(__file__), "..", "..", "pdf_samples", "v2")
+
+
+@pytest.fixture
+def template_stream(pdf_samples):
+    with open(os.path.join(pdf_samples, "sample_template.pdf"), "rb+") as f:
+        return f.read()
+
+
+@pytest.fixture
+def image_stream(pdf_samples):
+    with open(os.path.join(pdf_samples, "sample_image.jpg"), "rb+") as f:
+        return f.read()
+
+
+def test_draw_image_on_one_page(template_stream, image_stream, pdf_samples):
+    with open(os.path.join(pdf_samples, "sample_pdf_with_image.pdf"), "rb+") as f:
+        obj = PyPDFForm(template_stream).draw_image(image_stream, 2, 100, 100, 400, 300)
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
