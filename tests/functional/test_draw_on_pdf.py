3608395e721a1425f4270423d85f2bbcd967ec9a
chinapandaman <benjaminli801@gmail.com>
2021-01-21 15:26:15 -0600

    PPF-141: test draw text different font

--- tests/functional/test_draw_on_pdf.py
+++ tests/functional/test_draw_on_pdf.py
@@ -14,6 +14,11 @@ def pdf_samples():
 
 
 @pytest.fixture
+def font_samples():
+    return os.path.join(os.path.dirname(__file__), "..", "..", "font_samples")
+
+
+@pytest.fixture
 def template_stream(pdf_samples):
     with open(os.path.join(pdf_samples, "sample_template.pdf"), "rb+") as f:
         return f.read()
@@ -29,6 +34,21 @@ def test_draw_text_on_one_page(template_stream, pdf_samples):
     with open(os.path.join(pdf_samples, "sample_pdf_with_drawn_text.pdf"), "rb+") as f:
         obj = PyPDFForm(template_stream).draw_text(
             "drawn_text", 1, 300, 225, TextConstants().global_font, 20, (1, 0, 0), 50, 50, 4
+        )
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
+
+
+def test_draw_text_on_one_page_different_font(template_stream, pdf_samples, font_samples):
+    with open(os.path.join(font_samples, "LiberationSerif-BoldItalic.ttf"), "rb+") as _f:
+        PyPDFForm.register_font("LiberationSerif-BoldItalic", _f.read())
+
+    with open(os.path.join(pdf_samples, "sample_pdf_with_drawn_text_different_font.pdf"), "rb+") as f:
+        obj = PyPDFForm(template_stream).draw_text(
+            "drawn_text", 1, 300, 225, "LiberationSerif-BoldItalic", 20, (1, 0, 0), 50, 50, 4
         )
 
         expected = f.read()
