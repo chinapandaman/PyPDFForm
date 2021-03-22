9fbe3d24bdb72d802c8f5776dc42f836410301bd
chinapandaman <b.j.li@foxmail.com>
2021-03-22 17:26:39 -0500

    PPF-174: change stream to read

--- examples/draw_text.py
+++ examples/draw_text.py
@@ -14,4 +14,4 @@ with open(PATH_TO_DOWNLOADED_SAMPLE_PDF_FORM, "rb+") as template:
     filled_pdf = PyPDFForm(template.read()).draw_text("drawn_text", 1, 300, 225)
 
     with open(PATH_TO_FILLED_PDF_FORM, "wb+") as output:
-        output.write(filled_pdf.stream)
+        output.write(filled_pdf.read())
