51284bbe18d14ecedc54e7fedfd100701cb1a1e3
chinapandaman <b.j.li@foxmail.com>
2021-03-22 17:56:33 -0500

    PPF-170: check file extension

--- tests/functional/scenario/test_conftest.py
+++ tests/functional/scenario/test_conftest.py
@@ -6,3 +6,6 @@ import os
 def test_fixture_setup(pdf_directory, sample_job_application):
     assert os.path.isdir(pdf_directory)
     assert os.path.isfile(sample_job_application)
+
+    file_extension = os.path.splitext(sample_job_application)[1]
+    assert file_extension == ".pdf"
