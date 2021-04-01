21a59ac6e04a1879a6493fd635edb2953bf0535c
chinapandaman <b.j.li@foxmail.com>
2021-04-01 17:27:41 -0500

    PPF-178: finish adapting template

--- PyPDFForm/middleware/adapter.py
+++ PyPDFForm/middleware/adapter.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Contains user input adapters."""
 
+import os
 from typing import Union, BinaryIO
 
 
@@ -8,12 +9,18 @@ class FileAdapter:
     """Contains methods for adapting user inputs for files."""
 
     @staticmethod
-    def fp_or_f_obj_or_stream_to_stream(fp_or_f_obj_or_stream: Union[str, BinaryIO]) -> bytes:
+    def fp_or_f_obj_or_stream_to_stream(fp_or_f_obj_or_stream: Union[str, BinaryIO]) -> Union[bytes, None]:
         if isinstance(fp_or_f_obj_or_stream, bytes):
             return fp_or_f_obj_or_stream
 
         if callable(getattr(fp_or_f_obj_or_stream, "read", None)):
             return fp_or_f_obj_or_stream.read()
 
-        with open(fp_or_f_obj_or_stream, "rb+") as f:
-            return f.read()
+        if isinstance(fp_or_f_obj_or_stream, str):
+            if not os.path.isfile(fp_or_f_obj_or_stream):
+                return None
+
+            with open(fp_or_f_obj_or_stream, "rb+") as f:
+                return f.read()
+
+        return None
