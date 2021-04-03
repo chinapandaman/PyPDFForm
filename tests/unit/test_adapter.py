eae012ef0e51e2cd7761061828bb4ddd04699ebf
chinapandaman <b.j.li@foxmail.com>
2021-04-03 16:53:51 -0500

    PPF-178: unit test file adapter

---
+++ tests/unit/test_adapter.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pytest
+
+from PyPDFForm.middleware.adapter import FileAdapter
+
+
+@pytest.fixture
+def pdf_samples():
+    return os.path.join(os.path.dirname(__file__), "..", "..", "pdf_samples", "v2")
+
+
+@pytest.fixture
+def template_stream(pdf_samples):
+    with open(os.path.join(pdf_samples, "sample_template.pdf"), "rb+") as f:
+        return f.read()
+
+
+def test_file_adapter_fp_or_f_obj_or_stream_to_stream(pdf_samples, template_stream):
+    path = os.path.join(pdf_samples, "sample_template.pdf")
+    assert FileAdapter().fp_or_f_obj_or_stream_to_stream(path) == template_stream
+    with open(path, "rb+") as f:
+        assert FileAdapter().fp_or_f_obj_or_stream_to_stream(f) == template_stream
