572b28389f312883de146537e98df4dcad763706
chinapandaman <b.j.li@foxmail.com>
2021-04-04 21:09:42 -0500

    PPF-190: correct implementation

--- PyPDFForm/core/image.py
+++ PyPDFForm/core/image.py
@@ -63,8 +63,9 @@ class Image:
             buff.close()
             return image_stream
 
+        rgb_image = image.convert("RGB")
         with BytesIO() as f:
-            image.save(f, format="JPEG")
+            rgb_image.save(f, format="JPEG")
             f.seek(0)
             result = f.read()
 
