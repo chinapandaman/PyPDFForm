0d91b09bc7b0fd9b3f5cd4ad1a85b74bbbe2f641
chinapandaman <b.j.li@foxmail.com>
2021-11-14 14:23:36 -0600

    PPF-252: also reorganized scenario types

--- tests/functional/scenario/test_issues.py
+++ tests/functional/scenario/test_issues.py
@@ -5,19 +5,19 @@ import os
 from PyPDFForm import PyPDFForm, PyPDFForm2
 
 
-def test_pdf_form_with_pages_without_elements(pdf_directory):
-    obj = PyPDFForm2(os.path.join(pdf_directory, "PPF-246.pdf")).fill(
+def test_pdf_form_with_pages_without_elements(issue_pdf_directory):
+    obj = PyPDFForm2(os.path.join(issue_pdf_directory, "PPF-246.pdf")).fill(
         {"QCredit": "5000.63"}
     )
 
-    with open(os.path.join(pdf_directory, "PPF-246-expected.pdf"), "rb+") as f:
+    with open(os.path.join(issue_pdf_directory, "PPF-246-expected.pdf"), "rb+") as f:
         assert obj.read() == f.read()
 
 
-def test_pdf_form_with_pages_without_elements_v1(pdf_directory):
+def test_pdf_form_with_pages_without_elements_v1(issue_pdf_directory):
     obj = PyPDFForm(
-        os.path.join(pdf_directory, "PPF-246.pdf"), simple_mode=False, sejda=True
+        os.path.join(issue_pdf_directory, "PPF-246.pdf"), simple_mode=False, sejda=True
     ).fill({"QCredit": "5000.63"})
 
-    with open(os.path.join(pdf_directory, "PPF-246-expected-v1.pdf"), "rb+") as f:
+    with open(os.path.join(issue_pdf_directory, "PPF-246-expected-v1.pdf"), "rb+") as f:
         assert obj.read() == f.read()
