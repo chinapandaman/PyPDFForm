bfc4a8046b2cfd94b41fd593fa7044b869467a8a
liuxinyu <xinyu6@illinois.edu>
2021-12-08 18:07:00 -0600

    flaky

--- tests/unit/test_font.py
+++ tests/unit/test_font.py
@@ -15,11 +15,12 @@ def font_samples():
 def test_register_bad_font():
     assert not FontCore().register_font("foo", b"bar")
 
-
-def test_register_font_and_is_registered(font_samples):
+def test_not_registered():
     assert not FontCore().is_registered("LiberationSerif")
 
+def test_register_font_and_is_registered(font_samples):
     with open(os.path.join(font_samples, "LiberationSerif-Regular.ttf"), "rb+") as f:
         FontCore().register_font("LiberationSerif", f.read())
 
         assert FontCore().is_registered("LiberationSerif")
+
