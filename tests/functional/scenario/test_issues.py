73148237884db7b86232dfe614e5538e4468477b
chinapandaman <benjaminli801@gmail.com>
2022-11-26 17:22:38 -0600

    PPF-285: add test

--- tests/functional/scenario/test_issues.py
+++ tests/functional/scenario/test_issues.py
@@ -25,3 +25,18 @@ def test_pdf_form_with_pages_without_elements_v1(issue_pdf_directory):
         expected = f.read()
         assert len(obj.read()) == len(expected)
         assert obj.read() == expected
+
+
+def test_pdf_form_with_central_aligned_text_fields(issue_pdf_directory):
+    obj = PyPDFForm2(
+        os.path.join(issue_pdf_directory, "PPF-285.pdf")
+    ).fill({
+        "name": "Hans Mustermann",
+        "fulladdress": "Musterstr. 12, 82903 Musterdorf, Musterland",
+        "advisorname": "Karl Test"
+    })
+
+    with open(os.path.join(issue_pdf_directory, "PPF-285-expected.pdf"), "rb+") as f:
+        expected = f.read()
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
