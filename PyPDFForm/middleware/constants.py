9056039f543cd6901eb9f11ab1a37219443ef0af
chinapandaman <benjaminli801@gmail.com>
2023-01-11 23:07:37 -0600

    PPF-317: remove imports

--- PyPDFForm/middleware/constants.py
+++ PyPDFForm/middleware/constants.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 """Contains constants for middleware layer."""
 
-from typing import Tuple, Union
-
-
 GLOBAL_FONT = "Helvetica"
 GLOBAL_FONT_SIZE = 12
 GLOBAL_FONT_COLOR = (0, 0, 0)
