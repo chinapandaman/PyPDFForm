7269160177edceb2e9bd2c99e7d7409ebc636c37
chinapandaman <benjaminli801@gmail.com>
2023-01-17 19:39:36 -0600

    PPF-322: update version requirement

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=dependencies,
 )
