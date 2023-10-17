1c307da63b4246edb461d3f2037e754fb1803a63
chinapandaman <benjaminli801@gmail.com>
2023-10-17 17:58:47 -0500

    PPF-386: still need to add test

--- PyPDFForm/core/font.py
+++ PyPDFForm/core/font.py
@@ -3,6 +3,7 @@
 
 import pdfrw
 from io import BytesIO
+import re
 
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFError, TTFont
@@ -32,16 +33,30 @@ def auto_detect_font(element: pdfrw.PdfDict) -> str:
     """Returns the font of the text field if it is one of the standard fonts."""
 
     result = constants.DEFAULT_FONT
+
     for pattern in TEXT_FIELD_APPEARANCE_PATTERNS:
         text_appearance = traverse_pattern(pattern, element)
+
         if text_appearance:
             text_appearance = (
                 text_appearance.replace("(", "").replace(")", "").split(" ")
             )
+
             for each in text_appearance:
                 if each.startswith("/"):
+                    text_segments = re.findall('[A-Z][^A-Z]*', each.replace("/", ""))
+
                     for font in pdfmetrics.standardFonts:
-                        if font.startswith(each.replace("/", "")):
+                        font_segments = re.findall('[A-Z][^A-Z]*', font.replace("-", ""))
+                        if len(font_segments) != len(text_segments):
+                            continue
+
+                        found = True
+                        for i, val in enumerate(font_segments):
+                            if not val.startswith(text_segments[i]):
+                                found = False
+
+                        if found:
                             return font
 
     return result
