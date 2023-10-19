f6466085dacf461c6d522c47d4be26ca1bf55256
jingeli <benjaminli801@gmail.com>
2023-10-19 17:23:27 -0500

    PPF-387: add more tests

--- tests/test_preview.py
+++ tests/test_preview.py
@@ -31,3 +31,45 @@ def test_preview_sejda(sejda_template, pdf_samples, request):
 
         assert len(preview) == len(expected)
         assert preview == expected
+
+
+def test_preview_paragraph_complex(sample_template_paragraph_complex, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "preview", "test_preview_paragraph_complex.pdf")
+    with open(expected_path, "rb+") as f:
+        preview = PyPDFForm(sample_template_paragraph_complex).preview
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = preview
+
+        expected = f.read()
+
+        assert len(preview) == len(expected)
+        assert preview == expected
+
+
+def test_preview_sejda_complex(sejda_template_complex, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "preview", "test_preview_sejda_complex.pdf")
+    with open(expected_path, "rb+") as f:
+        preview = PyPDFForm(sejda_template_complex).preview
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = preview
+
+        expected = f.read()
+
+        assert len(preview) == len(expected)
+        assert preview == expected
+
+
+def test_preview_comb_text_field(sample_template_with_comb_text_field, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "preview", "test_preview_comb_text_field.pdf")
+    with open(expected_path, "rb+") as f:
+        preview = PyPDFForm(sample_template_with_comb_text_field).preview
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = preview
+
+        expected = f.read()
+
+        assert len(preview) == len(expected)
+        assert preview == expected
