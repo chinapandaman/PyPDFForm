3329052be73f8cfabe87eb1b50c3a7969bfae825
chinapandaman <benjaminli801@gmail.com>
2024-01-19 17:30:11 -0600

    PPF-449: finish testing checkbox

--- tests/test_create_widget.py
+++ tests/test_create_widget.py
@@ -24,3 +24,68 @@ def test_create_checkbox_default(template_stream, pdf_samples, request):
 
         assert len(obj.stream) == len(expected)
         assert obj.stream == expected
+
+
+def test_create_checkbox_default_filled(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "widget", "create_checkbox_default_filled.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).create_widget(
+            "checkbox",
+            "foo",
+            1,
+            100,
+            100,
+        )
+        obj.fill(obj.sample_data)
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
+
+
+def test_create_checkbox_complex(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "widget", "create_checkbox_complex.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).create_widget(
+            "checkbox",
+            "foo",
+            1,
+            100,
+            100,
+            size=100
+        )
+        assert obj.schema["properties"]["foo"]["type"] == "boolean"
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
+
+
+def test_create_checkbox_complex_filled(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "widget", "create_checkbox_complex_filled.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).create_widget(
+            "checkbox",
+            "foo",
+            1,
+            100,
+            100,
+            size=100
+        )
+        obj.fill(obj.sample_data)
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
