a46490b8cf2431ebecb3e66486d19582eb2a8031
jingeli <benjaminli801@gmail.com>
2024-01-19 17:12:37 -0600

    PPF-449: start writing tests

---
+++ tests/test_create_widget.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from PyPDFForm import PdfWrapper
+
+
+def test_create_checkbox_default(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "widget", "create_checkbox_default.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).create_widget(
+            "checkbox",
+            "foo",
+            1,
+            100,
+            100,
+        )
+        assert obj.schema["properties"]["foo"]["type"] == "boolean"
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
