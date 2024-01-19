91a54c8be6b844bcf6a03059398088e7413ed736
chinapandaman <benjaminli801@gmail.com>
2024-01-19 17:42:37 -0600

    PPF-449: finish testing text

--- tests/test_create_widget.py
+++ tests/test_create_widget.py
@@ -89,3 +89,100 @@ def test_create_checkbox_complex_filled(template_stream, pdf_samples, request):
 
         assert len(obj.stream) == len(expected)
         assert obj.stream == expected
+
+
+def test_create_text_default(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "widget", "create_text_default.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).create_widget(
+            "text",
+            "foo",
+            1,
+            100,
+            100,
+        )
+        assert obj.schema["properties"]["foo"]["type"] == "string"
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
+
+
+def test_create_text_default_filled(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "widget", "create_text_default_filled.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).create_widget(
+            "text",
+            "foo",
+            1,
+            100,
+            100,
+        )
+        obj.fill(obj.sample_data)
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
+
+
+def test_create_text_complex(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "widget", "create_text_complex.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).create_widget(
+            "text",
+            "foo",
+            1,
+            100,
+            100,
+            width=400,
+            height=400,
+            max_length=2,
+            font="Times-Roman",
+            font_size=50,
+            font_color=(1, 0.5, 1)
+        )
+        assert obj.schema["properties"]["foo"]["type"] == "string"
+        assert obj.schema["properties"]["foo"]["maxLength"] == 2
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
+
+
+def test_create_text_complex_filled(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "widget", "create_text_complex_filled.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).create_widget(
+            "text",
+            "foo",
+            1,
+            100,
+            100,
+            width=400,
+            height=400,
+            max_length=2,
+            font="Times-Roman",
+            font_size=50,
+            font_color=(1, 0.5, 1)
+        )
+        obj.fill(obj.sample_data)
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
