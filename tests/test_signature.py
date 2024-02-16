b1d01887f7283b3b04e03df06974820d7893fc94
chinapandaman <benjaminli801@gmail.com>
2024-02-15 21:14:54 -0800

    PPF-502: might as well test overlap

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -36,3 +36,23 @@ def test_signature_sample_value(pdf_samples):
 
     assert obj.widgets["signature"].sample_value == os.path.expanduser(
             "~/Downloads/sample_image.jpg")
+
+
+def test_fill_signature_overlap(pdf_samples, image_samples, request):
+    expected_path = os.path.join(
+            pdf_samples, "signature", "test_fill_signature_overlap.pdf"
+            )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(
+                os.path.join(pdf_samples, "signature", "sample_template_with_signature_overlap.pdf")
+                ).fill(
+                        {"signature": os.path.join(image_samples, "sample_signature.png")}
+                        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
