dcd08a10b738bb5606088d05f277bf175b661fee
chinapandaman <benjaminli801@gmail.com>
2024-02-16 22:06:26 -0600

    PPF-511: only allowed on PPF branches

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -2,10 +2,11 @@ if [[ "$VIRTUAL_ENV" == "" ]]; then
   source "./venv/bin/activate"
 fi
 
-python ./scripts/bump_version.py
-
-git add ./PyPDFForm/__init__.py mkdocs.yml
 BRANCH=$(git symbolic-ref HEAD 2>/dev/null)
 BRANCH=${BRANCH##refs/heads/}
-git commit -m "${BRANCH}: bump version"
-git push --set-upstream origin ${BRANCH}
+
+if python ./scripts/bump_version.py ${BRANCH} ; then
+  git add ./PyPDFForm/__init__.py mkdocs.yml
+  git commit -m "${BRANCH}: bump version"
+  git push --set-upstream origin ${BRANCH}
+fi
