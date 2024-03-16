addcd97eb0ad62c2b3b9666ff0c8af8644af1026
chinapandaman <benjaminli801@gmail.com>
2024-03-16 16:36:18 -0500

    PPF-522: test_filling_pdf_escape_pdf_form

---
+++ tests/scenario/test_tools_simple.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from PyPDFForm import FormWrapper
+
+
+def test_filling_pdf_escape_pdf_form(tool_pdf_directory, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "simple", "scenario", "tools", "pdf_escape_expected.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(os.path.join(tool_pdf_directory, "pdf_escape.pdf")).fill(
+            {
+                "test_1": "test_1",
+                "test_2": "test_2",
+                "test_3": "test_3",
+                "check_1": True,
+                "check_2": True,
+                "check_3": True,
+                "radio_1": 2,
+            }
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.stream == expected
