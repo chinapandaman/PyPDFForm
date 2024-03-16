a0b148497422181759ef9a97d8b12efe654f66c3
chinapandaman <benjaminli801@gmail.com>
2024-03-16 16:18:25 -0500

    PPF-522: test_dropdown_four

--- tests/test_dropdown_simple.py
+++ tests/test_dropdown_simple.py
@@ -96,3 +96,28 @@ def test_dropdown_three(sample_template_with_dropdown, pdf_samples, request):
 
         assert len(obj.read()) == len(expected)
         assert obj.stream == expected
+
+
+def test_dropdown_four(sample_template_with_dropdown, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "simple", "dropdown", "dropdown_four.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(sample_template_with_dropdown).fill(
+            {
+                "test_1": "test_1",
+                "test_2": "test_2",
+                "test_3": "test_3",
+                "check_1": True,
+                "check_2": True,
+                "check_3": True,
+                "radio_1": 1,
+                "dropdown_1": 3,
+            },
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.stream == expected
