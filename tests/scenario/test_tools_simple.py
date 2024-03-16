93a8c7c216a9d61fda7f880d81bc558c3c0f3649
chinapandaman <benjaminli801@gmail.com>
2024-03-16 16:39:59 -0500

    PPF-522: test_filling_docfly_pdf_form

--- tests/scenario/test_tools_simple.py
+++ tests/scenario/test_tools_simple.py
@@ -27,3 +27,27 @@ def test_filling_pdf_escape_pdf_form(tool_pdf_directory, pdf_samples, request):
 
         assert len(obj.read()) == len(expected)
         assert obj.stream == expected
+
+
+def test_filling_docfly_pdf_form(tool_pdf_directory, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "simple", "scenario", "tools", "docfly_expected.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(os.path.join(tool_pdf_directory, "docfly.pdf")).fill(
+            {
+                "test_1": "test_1",
+                "test_2": "test_2",
+                "test_3": "test_3",
+                "check_1": True,
+                "check_2": True,
+                "check_3": True,
+                "radio_1": 1,
+            }
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.stream == expected
