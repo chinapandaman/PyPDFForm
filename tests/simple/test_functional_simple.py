98967be26d94cc22e259dc4ec01e67a4979005d4
chinapandaman <benjaminli801@gmail.com>
2024-03-16 11:21:16 -0500

    PPF-522: test_fill_font_color

--- tests/simple/test_functional_simple.py
+++ tests/simple/test_functional_simple.py
@@ -63,3 +63,24 @@ def test_fill_right_aligned(
 
         assert len(obj.read()) == len(expected)
         assert obj.stream == expected
+
+
+def test_fill_font_color(sample_template_with_font_colors, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "simple", "test_fill_font_color.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(sample_template_with_font_colors).fill(
+            {
+                "red_12": "red",
+                "green_14": "green",
+                "blue_16": "blue",
+                "mixed_auto": "mixed",
+            },
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.stream == expected
