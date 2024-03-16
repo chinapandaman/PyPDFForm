bcda64a21071adc3b99c1c3e8370736fb8d3e8f1
chinapandaman <benjaminli801@gmail.com>
2024-03-16 12:08:06 -0500

    PPF-522: test_fill_max_length_text_field_odd_chars

--- tests/test_fill_max_length_text_field_simple.py
+++ tests/test_fill_max_length_text_field_simple.py
@@ -23,8 +23,31 @@ def test_fill_max_length_text_field_all_chars(
 
         request.config.results["expected_path"] = expected_path
         request.config.results["stream"] = obj.read()
-        assert len(obj.read()) == len(obj.stream)
-        assert obj.read() == obj.stream
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
+
+
+def test_fill_max_length_text_field_odd_chars(
+    sample_template_with_max_length_text_field, pdf_samples, request
+):
+    expected_path = os.path.join(pdf_samples, "simple", "max_length_text_field_related",
+                                 "max_length_text_field_odd_chars.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(sample_template_with_max_length_text_field).fill(
+            {
+                "FirstName": "John",
+                "MiddleName": "Joe",
+                "LastName": "XXX",
+                "Awesomeness": True,
+                "Gender": 0,
+            }
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
 
         expected = f.read()
 
