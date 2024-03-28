b9cc4325d9a4086c5cfd1d6e0df9a85520a49877
chinapandaman <benjaminli801@gmail.com>
2024-03-27 20:05:23 -0500

    PPF-539: start adding tests

--- tests/test_fill_max_length_text_field_simple.py
+++ tests/test_fill_max_length_text_field_simple.py
@@ -90,3 +90,32 @@ def test_fill_max_length_text_field_even_chars(
 
         assert len(obj.stream) == len(expected)
         assert obj.stream == expected
+
+
+def test_fill_comb_text_field_all_chars(
+    sample_template_with_comb_text_field, pdf_samples, request
+):
+    expected_path = os.path.join(
+        pdf_samples,
+        "simple",
+        "max_length_text_field_related",
+        "comb_text_field_all_chars.pdf",
+    )
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(sample_template_with_comb_text_field).fill(
+            {
+                "FirstName": "John",
+                "MiddleName": "Joe",
+                "LastName": "XXXXXXXXXX",
+                "Awesomeness": True,
+                "Gender": 0,
+            }
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
