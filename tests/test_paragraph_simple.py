4a828ff9fe3f43dbe152986f7153c1a31ed6308e
chinapandaman <benjaminli801@gmail.com>
2024-03-28 20:20:01 -0500

    PPF-547: add new tests

--- tests/test_paragraph_simple.py
+++ tests/test_paragraph_simple.py
@@ -88,6 +88,115 @@ def test_paragraph_auto_font_auto_wrap(
             assert obj.stream == expected
 
 
+def test_fill_sejda_complex(sejda_template_complex, pdf_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "simple", "paragraph", "sample_filled_sejda_complex.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(sejda_template_complex).fill(
+            {
+                "checkbox": True,
+                "radio": 0,
+                "dropdown_font_auto_left": 0,
+                "dropdown_font_auto_center": 1,
+                "dropdown_font_auto_right": 2,
+                "dropdown_font_ten_left": 0,
+                "dropdown_font_ten_center": 1,
+                "dropdown_font_ten_right": 2,
+                "paragraph_font_auto_left": "paragraph_font_auto_left",
+                "paragraph_font_auto_center": "paragraph_font_auto_center",
+                "paragraph_font_auto_right": "paragraph_font_auto_right",
+                "paragraph_font_ten_left": "paragraph_font_ten_left",
+                "paragraph_font_ten_center": "paragraph_font_ten_center",
+                "paragraph_font_ten_right": "paragraph_font_ten_right",
+                "text__font_auto_left": "test text",
+                "text_font_auto_center": "test text",
+                "text_font_auto_right": "test text",
+                "text_font_ten_left": "text_font_ten_left",
+                "text_font_ten_center": "text_font_ten_center",
+                "text_font_ten_right": "text_font_ten_right",
+            }
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.stream == expected
+
+
+def test_fill_sejda_complex_flatten(sejda_template_complex, pdf_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "simple", "paragraph", "sample_filled_sejda_complex_flatten.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(sejda_template_complex).fill(
+            {
+                "checkbox": True,
+                "radio": 0,
+                "dropdown_font_auto_left": 0,
+                "dropdown_font_auto_center": 1,
+                "dropdown_font_auto_right": 2,
+                "dropdown_font_ten_left": 0,
+                "dropdown_font_ten_center": 1,
+                "dropdown_font_ten_right": 2,
+                "paragraph_font_auto_left": "paragraph_font_auto_left",
+                "paragraph_font_auto_center": "paragraph_font_auto_center",
+                "paragraph_font_auto_right": "paragraph_font_auto_right",
+                "paragraph_font_ten_left": "paragraph_font_ten_left",
+                "paragraph_font_ten_center": "paragraph_font_ten_center",
+                "paragraph_font_ten_right": "paragraph_font_ten_right",
+                "text__font_auto_left": "test text",
+                "text_font_auto_center": "test text",
+                "text_font_auto_right": "test text",
+                "text_font_ten_left": "text_font_ten_left",
+                "text_font_ten_center": "text_font_ten_center",
+                "text_font_ten_right": "text_font_ten_right",
+            },
+            flatten=True,
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.stream == expected
+
+
+def test_sejda_complex_paragraph_multiple_line_alignment(
+    sejda_template_complex, pdf_samples, request
+):
+    expected_path = os.path.join(
+        pdf_samples,
+        "simple",
+        "paragraph",
+        "sample_filled_sejda_complex_paragraph_multiple_line_alignment.pdf",
+    )
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(sejda_template_complex).fill(
+            {
+                "paragraph_font_auto_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_auto_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_auto_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_ten_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_ten_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_ten_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            }
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.stream == expected
+
+
 def test_paragraph_complex(sample_template_paragraph_complex, pdf_samples, request):
     expected_path = os.path.join(
         pdf_samples, "simple", "paragraph", "test_paragraph_complex.pdf"
