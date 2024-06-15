2328255d2ff5e2981c79e64ed063d9271699723d
Jinge Li <9894243+chinapandaman@users.noreply.github.com>
2024-06-15 03:58:11 +0000

    PPF-649: test_dropdown_one

--- tests/test_adobe_mode.py
+++ tests/test_adobe_mode.py
@@ -5,6 +5,32 @@ import os
 from PyPDFForm import FormWrapper
 
 
+def test_dropdown_one(sample_template_with_dropdown, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "adobe_mode", "dropdown", "dropdown_one.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = FormWrapper(sample_template_with_dropdown).fill(
+            {
+                "test_1": "test_1",
+                "test_2": "test_2",
+                "test_3": "test_3",
+                "check_1": True,
+                "check_2": True,
+                "check_3": True,
+                "radio_1": 1,
+                "dropdown_1": 0,
+            },
+            adobe_mode=True,
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.stream == expected
+
+
 def test_fill_sejda_complex(sejda_template_complex, pdf_samples, request):
     expected_path = os.path.join(
         pdf_samples, "adobe_mode", "paragraph", "sample_filled_sejda_complex.pdf"
