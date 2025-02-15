1a5a34c061abf0c3b06c559de0e58d0ede925cb0
chinapandaman <benjaminli801@gmail.com>
2025-02-14 20:43:38 -0600

    PPF-794: add tests

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -59,3 +59,71 @@ def test_fill_signature_overlap(pdf_samples, image_samples, request):
         if os.name != "nt":
             assert len(obj.read()) == len(expected)
             assert obj.read() == expected
+
+
+def test_fill_signature_overlap_not_preserve_aspect_ratio(pdf_samples, image_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "signature", "test_fill_signature_overlap_not_preserve_aspect_ratio.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(
+            os.path.join(
+                pdf_samples, "signature", "sample_template_with_signature_overlap.pdf"
+            )
+        )
+        obj.widgets["signature"].preserve_aspect_ratio = False
+        obj.fill({"signature": os.path.join(image_samples, "sample_signature.png")})
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        if os.name != "nt":
+            assert len(obj.read()) == len(expected)
+            assert obj.read() == expected
+
+
+def test_fill_small_icon(pdf_samples, image_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "signature", "test_fill_small_icon.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(
+            os.path.join(
+                pdf_samples, "signature", "sample_template_with_signature_overlap.pdf"
+            )
+        )
+        obj.fill({"signature": os.path.join(image_samples, "small_icon.png")})
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        if os.name != "nt":
+            assert len(obj.read()) == len(expected)
+            assert obj.read() == expected
+
+
+def test_fill_small_icon_not_preserve_aspect_ratio(pdf_samples, image_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "signature", "test_fill_small_icon_not_preserve_aspect_ratio.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(
+            os.path.join(
+                pdf_samples, "signature", "sample_template_with_signature_overlap.pdf"
+            )
+        )
+        obj.widgets["signature"].preserve_aspect_ratio = False
+        obj.fill({"signature": os.path.join(image_samples, "small_icon.png")})
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        if os.name != "nt":
+            assert len(obj.read()) == len(expected)
+            assert obj.read() == expected
