8fc1a8e190fad09b5267f418c8d32fde39903127
chinapandaman <benjaminli801@gmail.com>
2025-04-12 23:50:41 -0500

    PPF-870: update docstring

--- PyPDFForm/widgets/image.py
+++ PyPDFForm/widgets/image.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
+"""
+This module provides the ImageWidget class for creating and customizing
+image fields in PDF forms. It extends SignatureWidget to enable placement
+of image widgets on specified pages and coordinates, using an image field
+template instead of a signature field template.
+"""
 
 from .signature import SignatureWidget
 
 
 class ImageWidget(SignatureWidget):
+    """
+    A widget for adding an image field to a PDF form.
+
+    Inherits from SignatureWidget, but uses an image widget template
+    instead of a signature field template. All initialization parameters
+    and methods are inherited from SignatureWidget.
+
+    Attributes:
+        BEDROCK_WIDGET_TO_COPY (str): The widget type to copy from the bedrock template ("image").
+    """
+
     BEDROCK_WIDGET_TO_COPY = "image"
