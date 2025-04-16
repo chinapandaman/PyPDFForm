0814a86233c99b02c8d02a8722879d5b1d49dbb6
chinapandaman <benjaminli801@gmail.com>
2025-04-15 20:26:27 -0500

    PPF-875: add linting and coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,4 +4,10 @@ fi
 
 rm -rf htmlcov/
 coverage run -m pytest && coverage html
-google-chrome htmlcov/index.html
+
+if [ "$PYPDFFORM_ENV" == "container" ]; then
+  echo "Coverage report: http://localhost:8000/htmlcov/index.html"
+  bash ./scripts/http_server.sh > /dev/null 2>&1
+else
+  google-chrome htmlcov/index.html
+fi
