a1ad244dc77a4cb78d6747a22982a655d6d23959
chinapandaman <benjaminli801@gmail.com>
2025-04-15 22:37:43 -0500

    PPF-877: still has a bug

--- scripts/open_pdf_diff.py
+++ scripts/open_pdf_diff.py
@@ -6,16 +6,24 @@ import sys
 import webbrowser
 
 if __name__ == "__main__":
+    project_root = "PyPDFForm"
+    if os.environ.get("PYPDFFORM_ENV") == "container":
+        project_root = project_root.lower()
     before_path = sys.argv[1]
     file_name = "_".join(before_path.split("/"))
     after_path = os.path.join(os.path.dirname(__file__), "..", "temp", file_name)
 
-    if os.environ.get("CODESPACES") == "true":
+    if (
+        os.environ.get("CODESPACES") == "true"
+        or os.environ.get("PYPDFFORM_ENV") == "container"
+    ):
         base_url = "https://{}-8000.app.github.dev/".format(
             os.environ.get("CODESPACE_NAME")
         )
-        print("Before:", base_url + before_path.split("PyPDFForm/")[1])
-        print("After:", base_url + after_path.split("PyPDFForm/./scripts/../")[1])
+        if os.environ.get("PYPDFFORM_ENV") == "container":
+            base_url = "http://localhost:8000/"
+        print("Before:", base_url + before_path.split(f"{project_root}/")[1])
+        print("After:", base_url + after_path.split(f"{project_root}/./scripts/../")[1])
     else:
         webbrowser.get("/usr/bin/google-chrome %s").open(before_path)
         webbrowser.get("/usr/bin/google-chrome %s").open(after_path)
