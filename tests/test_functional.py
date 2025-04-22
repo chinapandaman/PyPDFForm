4df09dafcf988a7d57afe978d0e024e92264365a
chinapandaman <benjaminli801@gmail.com>
2025-04-22 18:51:41 -0500

    PPF-912: add new tests for draw image

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -307,6 +307,54 @@ def test_draw_image_on_one_page(template_stream, image_samples, pdf_samples, req
             assert obj.stream == expected
 
 
+def test_draw_image_on_radio_template(
+    template_with_radiobutton_stream, image_samples, pdf_samples, request
+):
+    expected_path = os.path.join(pdf_samples, "test_draw_image_on_radio_template.pdf")
+    with open(expected_path, "rb+") as f:
+        with open(os.path.join(image_samples, "sample_image.jpg"), "rb+") as _f:
+            obj = PdfWrapper(template_with_radiobutton_stream).draw_image(
+                _f,
+                2,
+                100,
+                100,
+                400,
+                225,
+            )
+
+        expected = f.read()
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
+
+
+def test_draw_image_on_sejda_template(
+    sejda_template, image_samples, pdf_samples, request
+):
+    expected_path = os.path.join(pdf_samples, "test_draw_image_on_sejda_template.pdf")
+    with open(expected_path, "rb+") as f:
+        with open(os.path.join(image_samples, "sample_image.jpg"), "rb+") as _f:
+            obj = PdfWrapper(sejda_template).draw_image(
+                _f,
+                2,
+                100,
+                100,
+                400,
+                225,
+            )
+
+        expected = f.read()
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        assert len(obj.stream) == len(expected)
+        assert obj.stream == expected
+
+
 def test_draw_png_image_on_one_page(
     template_stream, image_samples, pdf_samples, request
 ):
