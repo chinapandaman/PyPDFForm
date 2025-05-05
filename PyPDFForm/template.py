bb33b0f9622f9abf7da827c391fe6998e2437faf
chinapandaman <benjaminli801@gmail.com>
2025-05-05 17:31:54 -0500

    PPF-939: this is a bug

--- PyPDFForm/template.py
+++ PyPDFForm/template.py
@@ -266,7 +266,7 @@ def get_widget_full_key(widget: dict) -> Union[str, None]:
     ):
         return f"{widget[Parent].get_object()[T]}.{key}"
 
-    return None
+    return key
 
 
 def construct_widget(widget: dict, key: str) -> Union[WIDGET_TYPES, None]:
