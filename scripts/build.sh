5ae58994b2c5b832a48da0c7dad7714adb72954a
chinapandaman <benjaminli801@gmail.com>
2025-05-06 20:04:26 -0500

    PPF-943: also fix indentation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 if ! command -v uv &> /dev/null
 then
-    echo "uv not found, installing..."
-    wget -qO- https://astral.sh/uv/install.sh | sh
+  echo "uv not found, installing..."
+  wget -qO- https://astral.sh/uv/install.sh | sh
+  source $HOME/.local/bin/env
 else
     echo "uv is already installed."
 fi
