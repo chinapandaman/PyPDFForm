63664bcdcc32cda83ef9759c24b97c68d56724af
chinapandaman <benjaminli801@gmail.com>
2025-05-18 19:47:45 -0500

    PPF-973: add docstrings

--- PyPDFForm/middleware/base.py
+++ PyPDFForm/middleware/base.py
@@ -52,7 +52,23 @@ class Widget:
         self.render_widget = None
         self.hooks_to_trigger = []
 
-    def __setattr__(self, name: str, value: Any):
+    def __setattr__(self, name: str, value: Any) -> None:
+        """Sets an attribute on the widget with special handling for hook-triggering attributes.
+
+        For attributes listed in SET_ATTR_TRIGGER_HOOK_MAP, when set to non-None values,
+        adds a hook to hooks_to_trigger list with the mapped hook name and value.
+        All other attributes are set normally via the standard object.__setattr__.
+
+        Args:
+            name: Name of the attribute to set
+            value: Value to set the attribute to
+
+        Note:
+            The hook triggering only occurs when:
+            1. The attribute is in SET_ATTR_TRIGGER_HOOK_MAP
+            2. The value being set is not None
+        """
+
         if name in self.SET_ATTR_TRIGGER_HOOK_MAP and value is not None:
             self.hooks_to_trigger.append((self.SET_ATTR_TRIGGER_HOOK_MAP[name], value))
         super().__setattr__(name, value)
