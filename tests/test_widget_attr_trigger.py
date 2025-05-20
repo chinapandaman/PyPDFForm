5107eb5ac0464e72d2e1915cbd15386df3c07491
chinapandaman <benjaminli801@gmail.com>
2025-05-19 20:13:12 -0500

    PPF-979: test radio

--- tests/test_widget_attr_trigger.py
+++ tests/test_widget_attr_trigger.py
@@ -77,3 +77,39 @@ def test_set_checkbox_size_sejda(pdf_samples, sejda_template, request):
 
         assert len(obj.read()) == len(expected)
         assert obj.read() == expected
+
+
+def test_set_radio_size(pdf_samples, template_with_radiobutton_stream, request):
+    expected_path = os.path.join(
+        pdf_samples, "test_widget_attr_trigger", "test_set_radio_size.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_with_radiobutton_stream)
+        obj.TRIGGER_WIDGET_HOOKS = True
+        obj.widgets["radio_1"].size = 40
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
+
+
+def test_set_radio_size_sejda(pdf_samples, sejda_template, request):
+    expected_path = os.path.join(
+        pdf_samples, "test_widget_attr_trigger", "test_set_radio_size_sejda.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(sejda_template)
+        obj.TRIGGER_WIDGET_HOOKS = True
+        obj.widgets["purchase_option"].size = 40
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
