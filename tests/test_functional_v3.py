05fb6b6241141fe68da0b6f21291aea03d5ef5ca
chinapandaman <benjaminli801@gmail.com>
2025-05-22 17:56:05 -0500

    PPF-1002: v3 tests

---
+++ tests/test_functional_v3.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from PyPDFForm import PdfWrapper
+from PyPDFForm.middleware.base import Widget
+
+
+def test_base_schema_definition():
+    assert Widget("foo").schema_definition == {}
+
+
+def test_fill(template_stream, pdf_samples, data_dict, request):
+    expected_path = os.path.join(pdf_samples, "sample_filled.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).fill(
+            data_dict,
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+        assert len(obj.read()) == len(obj.read())
+        assert obj.read() == obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
