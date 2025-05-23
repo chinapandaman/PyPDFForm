19d989d0a9c2e3035514e34c892e522f8bfde03f
chinapandaman <benjaminli801@gmail.com>
2025-05-22 22:19:22 -0500

    PPF-1002: v3 tests

--- tests/test_paragraph_v3.py
+++ tests/test_paragraph_v3.py
@@ -308,3 +308,52 @@ def test_sejda_complex_paragraph_multiple_line_alignment_flatten(
 
         assert len(obj.read()) == len(expected)
         assert obj.read() == expected
+
+
+def test_paragraph_complex(sample_template_paragraph_complex, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "paragraph", "test_paragraph_complex.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(sample_template_paragraph_complex).fill(
+            {
+                "paragraph_font_auto_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_auto_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_auto_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_ten_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_ten_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_ten_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            }
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        if os.name != "nt":
+            assert len(obj.read()) == len(expected)
+            assert obj.read() == expected
+
+
+def test_paragraph_complex_flatten(sample_template_paragraph_complex, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "paragraph", "test_paragraph_complex_flatten.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(sample_template_paragraph_complex).fill(
+            {
+                "paragraph_font_auto_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_auto_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_auto_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_ten_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_ten_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+                "paragraph_font_ten_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            },
+            flatten=True
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        if os.name != "nt":
+            assert len(obj.read()) == len(expected)
+            assert obj.read() == expected
