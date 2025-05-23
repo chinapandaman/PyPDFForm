540477116c0883216278f86d07f5669314eae118
chinapandaman <benjaminli801@gmail.com>
2025-05-23 07:00:13 -0500

    PPF-1002: v3 tests

--- tests/test_create_widget_v3.py
+++ tests/test_create_widget_v3.py
@@ -85,3 +85,31 @@ def test_create_checkbox_default_filled_flatten(template_stream, pdf_samples, re
 
         assert len(obj.read()) == len(expected)
         assert obj.read() == expected
+
+
+def test_create_checkbox_complex(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "widget", "test_create_checkbox_complex.pdf")
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream).create_widget(
+            "checkbox",
+            "foo",
+            1,
+            100,
+            100,
+            size=100,
+            button_style="check",
+            tick_color=(0, 1, 0),
+            bg_color=(0, 0, 1),
+            border_color=(1, 0, 0),
+            border_width=5,
+        )
+
+        assert obj.schema["properties"]["foo"]["type"] == "boolean"
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
