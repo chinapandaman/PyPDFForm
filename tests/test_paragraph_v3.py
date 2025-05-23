8aa6384714449bb30f6f2cb7c98a191e6cb61519
chinapandaman <benjaminli801@gmail.com>
2025-05-22 21:59:11 -0500

    PPF-1002: v3 tests

---
+++ tests/test_paragraph_v3.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+
+import os
+
+from PyPDFForm import PdfWrapper
+
+
+def test_paragraph_y_coordinate(sample_template_with_paragraph, pdf_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "paragraph", "test_paragraph_y_coordinate.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(sample_template_with_paragraph).fill(
+            {"paragraph_1": "test paragraph"}
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
+
+
+def test_paragraph_y_coordinate_flatten(sample_template_with_paragraph, pdf_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "paragraph", "test_paragraph_y_coordinate_flatten.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(sample_template_with_paragraph).fill(
+            {"paragraph_1": "test paragraph"}, flatten=True,
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
