a829db82d1431b9d2360b5a204b22a06b246be58
chinapandaman <benjaminli801@gmail.com>
2025-05-24 11:29:05 -0500

    PPF-1017: finished PyPDFForm/__init__.py

--- PyPDFForm/__init__.py
+++ PyPDFForm/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = "2.5.0"
 
-from .middleware.text import Text
+from .middleware.text import Text  # exposing for setting global font attrs
 from .wrapper import PdfWrapper
 
 __all__ = ["PdfWrapper", "Text"]
