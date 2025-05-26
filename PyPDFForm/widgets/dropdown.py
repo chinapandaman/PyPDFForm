10e0e7c25b193ff3b37763a84c435d97fa27d5b2
chinapandaman <benjaminli801@gmail.com>
2025-05-26 14:57:49 -0500

    PPF-1032: finished widgets

--- PyPDFForm/widgets/dropdown.py
+++ PyPDFForm/widgets/dropdown.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
+"""
+This module defines the DropdownWidget class, which is a subclass of the
+TextWidget class. It represents a dropdown form field in a PDF document.
+"""
 
 from .text import TextWidget
 
 
 class DropdownWidget(TextWidget):
+    """
+    Represents a dropdown widget in a PDF form.
+
+    Inherits from the base TextWidget class and adds specific parameters for
+    dropdown styling, such as options.
+
+    Attributes:
+        NONE_DEFAULTS (list): A list of parameters that default to None.
+        ACRO_FORM_FUNC (str): The name of the AcroForm function to use for
+            creating the dropdown.
+    """
+
     NONE_DEFAULTS = []
     ACRO_FORM_FUNC = "_textfield"
 
@@ -15,6 +31,16 @@ class DropdownWidget(TextWidget):
         y: float,
         **kwargs,
     ) -> None:
+        """
+        Initializes a DropdownWidget object.
+
+        Args:
+            name (str): Name of the widget.
+            page_number (int): Page number of the widget.
+            x (float): X coordinate of the widget.
+            y (float): Y coordinate of the widget.
+            **kwargs: Additional keyword arguments.
+        """
         self.USER_PARAMS = super().USER_PARAMS[:-1] + [
             ("options", "options"),
         ]
