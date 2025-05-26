e7149fd6a6d7c86a893778dad17bf4ebead268ab
chinapandaman <benjaminli801@gmail.com>
2025-05-25 21:02:39 -0500

    PPF-1028: test coordinate docs

---
+++ tests/docs/test_coordinate.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from PyPDFForm import PdfWrapper
+
+
+def test_coordinate_grid_view(pdf_samples, template_stream, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_coordinate_grid_view.pdf")
+
+    grid_view_pdf = PdfWrapper(template_stream).generate_coordinate_grid(
+        color=(1, 0, 0),
+        margin=100,
+    )
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = grid_view_pdf.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(grid_view_pdf.read()) == len(expected)
+        assert grid_view_pdf.read() == expected
