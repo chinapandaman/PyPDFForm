9a15aca88c46f892120e8d47a1b4356a3c68013f
chinapandaman <benjaminli801@gmail.com>
2025-05-25 22:26:52 -0500

    PPF-1028: test draw docs

---
+++ tests/docs/test_draw.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from PyPDFForm import PdfWrapper
+
+
+def test_draw_text(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_draw_text.pdf")
+
+    pdf = PdfWrapper(template_stream).draw_text(
+        text="random text",
+        page_number=1,
+        x=300,
+        y=225,
+        font="Helvetica",  # optional
+        font_size=12,  # optional
+        font_color=(1, 0, 0),  # optional
+    )
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = pdf.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(pdf.read()) == len(expected)
+        assert pdf.read() == expected
+
+
+def test_draw_image(template_stream, image_samples, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_draw_image.pdf")
+
+    pdf = PdfWrapper(template_stream).draw_image(
+        image=os.path.join(image_samples, "sample_image.jpg"),
+        page_number=1,
+        x=100,
+        y=100,
+        width=400,
+        height=225,
+        rotation=0,  # optional
+    )
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = pdf.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(pdf.read()) == len(expected)
+        assert pdf.read() == expected
