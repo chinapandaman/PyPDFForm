27032657ab22c6e4c304854f9def826d48b355e0
chinapandaman <benjaminli801@gmail.com>
2025-05-26 14:41:55 -0500

    PPF-1028: finished middlewares

--- PyPDFForm/middleware/dropdown.py
+++ PyPDFForm/middleware/dropdown.py
@@ -1,20 +1,53 @@
 # -*- coding: utf-8 -*-
+"""
+Module representing a dropdown widget.
+
+This module defines the Dropdown class, which is a subclass of the
+Widget class. It represents a dropdown form field in a PDF document.
+"""
 
 from .base import Widget
 
 
 class Dropdown(Widget):
+    """
+    Represents a dropdown widget.
+
+    The Dropdown class provides a concrete implementation for
+    dropdown form fields. It inherits from the Widget class and
+    implements the schema_definition and sample_value properties.
+    """
+
     def __init__(
         self,
         name: str,
         value: int = None,
     ) -> None:
+        """
+        Initializes a dropdown widget.
+
+        Args:
+            name (str): The name of the dropdown.
+            value (int): The initial value of the dropdown. Defaults to None.
+
+        Attributes:
+            choices (List[str]): The list of choices for the dropdown.
+        """
         super().__init__(name, value)
 
         self.choices = []
 
     @property
     def schema_definition(self) -> dict:
+        """
+        Returns the schema definition for the dropdown.
+
+        The schema definition is a dictionary that describes the
+        data type and other constraints for the dropdown value.
+
+        Returns:
+            dict: A dictionary representing the schema definition.
+        """
         return {
             "type": "integer",
             "maximum": len(self.choices) - 1,
@@ -23,4 +56,13 @@ class Dropdown(Widget):
 
     @property
     def sample_value(self) -> int:
+        """
+        Returns a sample value for the dropdown.
+
+        The sample value is used to generate example data for the
+        dropdown field.
+
+        Returns:
+            int: A sample value for the dropdown.
+        """
         return len(self.choices) - 1
--- PyPDFForm/middleware/signature.py
+++ PyPDFForm/middleware/signature.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+"""
+Module representing a signature widget.
+
+This module defines the Signature class, which is a subclass of the
+Widget class. It represents a signature form field in a PDF document,
+allowing users to add their signature as an image.
+"""
 
 from os.path import expanduser
 from typing import Union
@@ -8,18 +15,57 @@ from .base import Widget
 
 
 class Signature(Widget):
+    """
+    Represents a signature widget.
+
+    The Signature class provides a concrete implementation for
+    signature form fields. It inherits from the Widget class and
+    implements the schema_definition, sample_value, and stream
+    properties.
+    """
+
     preserve_aspect_ratio = True
 
     @property
     def schema_definition(self) -> dict:
+        """
+        Returns the schema definition for the signature.
+
+        The schema definition is a dictionary that describes the
+        data type and other constraints for the signature value,
+        which is expected to be a string representing the path to
+        the signature image.
+
+        Returns:
+            dict: A dictionary representing the schema definition.
+        """
         return {"type": "string", **super().schema_definition}
 
     @property
     def sample_value(self) -> str:
+        """
+        Returns a sample value for the signature.
+
+        The sample value is used to generate example data for the
+        signature field. It returns the path to a sample image file.
+
+        Returns:
+            str: A sample value for the signature.
+        """
         return expanduser("~/Downloads/sample_image.jpg")
 
     @property
     def stream(self) -> Union[bytes, None]:
+        """
+        Returns the stream of the signature image.
+
+        This method reads the signature image from the file path
+        specified in the value attribute and returns the image data
+        as a stream of bytes.
+
+        Returns:
+            Union[bytes, None]: The stream of the signature image.
+        """
         return (
             fp_or_f_obj_or_stream_to_stream(self.value)
             if self.value is not None
