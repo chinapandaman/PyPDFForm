c9b1c6b9c2a54edb9c3b7d1e15d5a9623292b004
YangHeeChang <diogmlckd@naver.com>
2025-06-06 02:41:14 +0900

    add test korean

--- tests/multi_language/test_multi_language.py
+++ tests/multi_language/test_multi_language.py
@@ -24,3 +24,23 @@ def test_chinese_simplified(multi_language_pdf_samples, request):
 
         assert len(obj.read()) == len(expected)
         assert obj.read() == expected
+
+def test_korean_simplified(multi_language_pdf_samples, request):
+    expected_path = os.path.join(
+        multi_language_pdf_samples, "test_korean_simplified.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(os.path.join(multi_language_pdf_samples, "ko_kr.pdf")).fill(
+            {
+                "상호": "한국",
+                "성명": "홍길동",
+            },
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
