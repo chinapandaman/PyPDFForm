4488e7af31fd72c94a0b33561cfbd42904d7b52c
chinapandaman <benjaminli801@gmail.com>
2025-07-02 23:07:59 -0500

    PPF-1092: add dropdown tests

--- tests/test_dropdown.py
+++ tests/test_dropdown.py
@@ -290,6 +290,33 @@ def test_dropdown_alignment_flatten(dropdown_alignment, pdf_samples, request):
             assert obj.read() == expected
 
 
+def test_dropdown_alignment_flatten_then_unflatten(
+    dropdown_alignment, pdf_samples, request
+):
+    expected_path = os.path.join(
+        pdf_samples, "dropdown", "test_dropdown_alignment_flatten_then_unflatten.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(dropdown_alignment).fill(
+            {
+                "dropdown_left": 0,
+                "dropdown_center": 1,
+                "dropdown_right": 2,
+            },
+            flatten=True,
+        )
+        obj.widgets["dropdown_center"].readonly = False
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        if os.name != "nt":
+            assert len(obj.read()) == len(expected)
+            assert obj.read() == expected
+
+
 def test_dropdown_alignment_sejda(dropdown_alignment_sejda, pdf_samples, request):
     expected_path = os.path.join(
         pdf_samples, "dropdown", "test_dropdown_alignment_sejda.pdf"
@@ -333,6 +360,37 @@ def test_dropdown_alignment_sejda_flatten(
             },
             flatten=True,
         )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
+
+
+def test_dropdown_alignment_sejda_flatten_then_unflatten(
+    dropdown_alignment_sejda, pdf_samples, request
+):
+    expected_path = os.path.join(
+        pdf_samples,
+        "dropdown",
+        "test_dropdown_alignment_sejda_flatten_then_unflatten.pdf",
+    )
+    with open(
+        expected_path,
+        "rb+",
+    ) as f:
+        obj = PdfWrapper(dropdown_alignment_sejda).fill(
+            {
+                "dropdown_left": 0,
+                "dropdown_center": 1,
+                "dropdown_right": 2,
+            },
+            flatten=True,
+        )
+        obj.widgets["dropdown_center"].readonly = False
 
         request.config.results["expected_path"] = expected_path
         request.config.results["stream"] = obj.read()
