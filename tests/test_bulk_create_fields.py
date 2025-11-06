af82c3a82b06ae35655fee7be443bf64f9047dee
chinapandaman <benjaminli801@gmail.com>
2025-11-05 19:06:43 -0600

    PPF-1205: test all supported field types

--- tests/test_bulk_create_fields.py
+++ tests/test_bulk_create_fields.py
@@ -48,3 +48,54 @@ def test_bulk_create_fields_stress(template_stream, pdf_samples, request):
 
         assert len(obj.read()) == len(expected)
         assert obj.read() == expected
+
+
+@pytest.mark.posix_only
+def test_bulk_create_fields_stress_2(template_stream, pdf_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "bulk_create_fields", "test_bulk_create_fields_stress_2.pdf"
+    )
+
+    fields = []
+    x = 0
+    y = 0
+    margin = 25
+    while x <= 575:
+        while y <= 625:
+            fields.append(
+                Fields.TextField(f"text_{x}_{y}", 1, x, y, width=margin, height=margin)
+            )
+            fields.append(Fields.CheckBoxField(f"check_{x}_{y}", 2, x, y, size=margin))
+            fields.append(
+                Fields.DropdownField(
+                    f"dropdown_{x}_{y}",
+                    3,
+                    x,
+                    y,
+                    width=margin,
+                    height=margin,
+                    options=["foo", "bar"],
+                )
+            )
+            fields.append(
+                Fields.RadioGroup(
+                    f"radio_{x}_{y}", 4, [x, x + 12.5], [y, y + 12.5], size=12.5
+                )
+            )
+            y += margin
+        y = 0
+        x += margin
+
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(template_stream) + PdfWrapper(
+            os.path.join(pdf_samples, "dummy.pdf")
+        )
+        obj.bulk_create_fields(fields)
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
