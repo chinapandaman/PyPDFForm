b3605fb4b38a50c726bb078ef83fd1c2421a4c40
chinapandaman <benjaminli801@gmail.com>
2025-11-06 05:24:12 -0600

    PPF-1207: add test

--- tests/test_bulk_create_fields.py
+++ tests/test_bulk_create_fields.py
@@ -196,7 +196,7 @@ def test_bulk_create_fields_stress_max(pdf_samples, request):
             )
             fields.append(
                 Fields.SignatureField(
-                    f"image_{x}_{y}", 6, x, y, width=margin, height=margin
+                    f"signature_{x}_{y}", 6, x, y, width=margin, height=margin
                 )
             )
             y += margin
@@ -208,6 +208,84 @@ def test_bulk_create_fields_stress_max(pdf_samples, request):
         for _ in range(6):
             obj += PdfWrapper(os.path.join(pdf_samples, "dummy.pdf"))
         obj.bulk_create_fields(fields)
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+        assert obj.read() == expected
+
+
+@pytest.mark.posix_only
+def test_bulk_create_fields_stress_max_mixed(pdf_samples, request):
+    expected_path = os.path.join(
+        pdf_samples,
+        "bulk_create_fields",
+        "test_bulk_create_fields_stress_max_mixed.pdf",
+    )
+
+    fields = []
+    x = 0
+    y = 0
+    margin = 25
+    while x <= 450:
+        while y <= 625:
+            fields.append(
+                Fields.TextField(f"text_{x}_{y}", 1, x, y, width=margin, height=margin)
+            )
+            fields.append(
+                Fields.CheckBoxField(f"check_{x}_{y}", 1, x + margin, y, size=margin)
+            )
+            fields.append(
+                Fields.DropdownField(
+                    f"dropdown_{x}_{y}",
+                    1,
+                    x,
+                    y + margin,
+                    width=margin,
+                    height=margin,
+                    options=["foo", "bar"],
+                )
+            )
+            fields.append(
+                Fields.RadioGroup(
+                    f"radio_{x}_{y}",
+                    1,
+                    [x + margin, x + margin + 12.5],
+                    [y + margin, y + margin + 12.5],
+                    size=12.5,
+                )
+            )
+            fields.append(
+                Fields.ImageField(
+                    f"image_{x}_{y}",
+                    1,
+                    x + margin + margin,
+                    y,
+                    width=margin,
+                    height=margin,
+                )
+            )
+            fields.append(
+                Fields.SignatureField(
+                    f"signature_{x}_{y}",
+                    1,
+                    x + margin + margin,
+                    y + margin,
+                    width=margin,
+                    height=margin,
+                )
+            )
+            y += 50
+        y = 0
+        x += 75
+
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(os.path.join(pdf_samples, "dummy.pdf")).bulk_create_fields(
+            fields
+        )
 
         request.config.results["expected_path"] = expected_path
         request.config.results["stream"] = obj.read()
