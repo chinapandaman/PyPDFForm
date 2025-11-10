187f9bef6d5cc80d0489d0a5df041434e0c2471d
chinapandaman <benjaminli801@gmail.com>
2025-11-10 06:50:18 -0600

    PPF-1215: add docstrings

--- PyPDFForm/deprecation.py
+++ PyPDFForm/deprecation.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+"""
+A module for handling deprecation notices within the PyPDFForm library.
+
+This module provides utility functions to issue standard DeprecationWarning
+messages, ensuring consistency across the library when notifying users of
+deprecated features.
+"""
 
 from warnings import warn
 
@@ -6,6 +13,13 @@ from .constants import DEPRECATION_NOTICE
 
 
 def deprecation_notice(to_deprecate: str, to_replace: str) -> None:
+    """
+    Issues a DeprecationWarning for a feature that is being deprecated.
+
+    Args:
+        to_deprecate (str): The name of the feature or function being deprecated.
+        to_replace (str): The name of the feature or function that should be used instead.
+    """
     warn(
         DEPRECATION_NOTICE.format(
             to_deprecate,
