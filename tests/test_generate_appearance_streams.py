c1a82aab147a97a05feddb5c3870c79abde04d9a
chinapandaman <benjaminli801@gmail.com>
2025-11-12 06:56:53 -0600

    PPF-1217: finished most tests

--- tests/test_generate_appearance_streams.py
+++ tests/test_generate_appearance_streams.py
@@ -22,3 +22,95 @@ def test_fill(template_stream, pdf_samples, data_dict, request):
         expected = f.read()
 
         assert len(obj.read()) == len(expected)
+
+
+def test_dropdown_two(sample_template_with_dropdown, pdf_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "generate_appearance_streams", "dropdown", "dropdown_two.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(
+            sample_template_with_dropdown, generate_appearance_streams=True
+        ).fill(
+            {
+                "test_1": "test_1",
+                "test_2": "test_2",
+                "test_3": "test_3",
+                "check_1": True,
+                "check_2": True,
+                "check_3": True,
+                "radio_1": 1,
+                "dropdown_1": 1,
+            },
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+
+
+def test_fill_sejda_complex(sejda_template_complex, pdf_samples, request):
+    expected_path = os.path.join(
+        pdf_samples,
+        "generate_appearance_streams",
+        "paragraph",
+        "sample_filled_sejda_complex.pdf",
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(sejda_template_complex, generate_appearance_streams=True).fill(
+            {
+                "checkbox": True,
+                "radio": 0,
+                "dropdown_font_auto_left": 0,
+                "dropdown_font_auto_center": 1,
+                "dropdown_font_auto_right": 2,
+                "dropdown_font_ten_left": 0,
+                "dropdown_font_ten_center": 1,
+                "dropdown_font_ten_right": 2,
+                "paragraph_font_auto_left": "paragraph_font_auto_left",
+                "paragraph_font_auto_center": "paragraph_font_auto_center",
+                "paragraph_font_auto_right": "paragraph_font_auto_right",
+                "paragraph_font_ten_left": "paragraph_font_ten_left",
+                "paragraph_font_ten_center": "paragraph_font_ten_center",
+                "paragraph_font_ten_right": "paragraph_font_ten_right",
+                "text__font_auto_left": "test text",
+                "text_font_auto_center": "test text",
+                "text_font_auto_right": "test text",
+                "text_font_ten_left": "text_font_ten_left",
+                "text_font_ten_center": "text_font_ten_center",
+                "text_font_ten_right": "text_font_ten_right",
+            },
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+
+
+def test_issue_613(pdf_samples, request):
+    expected_path = os.path.join(
+        pdf_samples, "generate_appearance_streams", "issues", "613_expected.pdf"
+    )
+    with open(expected_path, "rb+") as f:
+        obj = PdfWrapper(
+            os.path.join(pdf_samples, "scenario", "issues", "613.pdf"),
+            generate_appearance_streams=True,
+        ).fill(
+            {
+                "301 Full name": "John Smith",
+                "301 Address Street": "1234 road number 6",
+            },
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
