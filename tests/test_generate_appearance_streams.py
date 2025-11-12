c2dbd9255d3035e0dab30e6ae2617cbf69f57494
chinapandaman <benjaminli801@gmail.com>
2025-11-12 07:08:00 -0600

    PPF-1217: this test needs to be revisited

--- tests/test_generate_appearance_streams.py
+++ tests/test_generate_appearance_streams.py
@@ -4,7 +4,9 @@
 
 import os
 
-from PyPDFForm import PdfWrapper
+import pytest
+
+from PyPDFForm import Fields, PdfWrapper
 
 
 def test_fill(template_stream, pdf_samples, data_dict, request):
@@ -107,6 +109,104 @@ def test_issue_613(pdf_samples, request):
                 "301 Address Street": "1234 road number 6",
             },
         )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = obj.read()
+
+        expected = f.read()
+
+        assert len(obj.read()) == len(expected)
+
+
+@pytest.mark.posix_only
+def test_sample_template_library(
+    pdf_samples, image_samples, sample_font_stream, request
+):
+    expected_path = os.path.join(
+        pdf_samples, "generate_appearance_streams", "test_sample_template_library.pdf"
+    )
+
+    with open(expected_path, "rb+") as f:
+        obj = (
+            PdfWrapper(
+                os.path.join(pdf_samples, "dummy.pdf"), generate_appearance_streams=True
+            )
+            .register_font("new_font", sample_font_stream)
+            .create_field(
+                Fields.TextField(
+                    name="new_text_field_widget",
+                    page_number=1,
+                    x=60,
+                    y=710,
+                )
+            )
+            .create_field(
+                Fields.CheckBoxField(
+                    name="new_checkbox_widget",
+                    page_number=1,
+                    x=100,
+                    y=600,
+                )
+            )
+            .create_field(
+                Fields.RadioGroup(
+                    name="new_radio_group",
+                    page_number=1,
+                    x=[50, 100, 150],
+                    y=[50, 100, 150],
+                )
+            )
+            .create_field(
+                Fields.DropdownField(
+                    name="new_dropdown_widget",
+                    page_number=1,
+                    x=300,
+                    y=710,
+                    options=[
+                        "foo",
+                        "bar",
+                        "foobar",
+                    ],
+                    font="new_font",
+                )
+            )
+            .create_field(
+                Fields.ImageField(
+                    name="new_image_widget",
+                    page_number=1,
+                    x=300,
+                    y=200,
+                )
+            )
+            .create_field(
+                Fields.SignatureField(
+                    name="new_signature_wiget",
+                    page_number=1,
+                    x=300,
+                    y=400,
+                )
+            )
+            .fill(
+                {
+                    "new_text_field_widget": "test text",
+                    "new_checkbox_widget": True,
+                    "new_radio_group": 1,
+                    "new_dropdown_widget": "barfoo",
+                    "new_image_widget": os.path.join(image_samples, "sample_image.jpg"),
+                    "new_signature_wiget": os.path.join(
+                        image_samples, "sample_signature.png"
+                    ),
+                },
+            )
+        )
+
+        obj.widgets["new_text_field_widget"].font = "new_font"
+        obj.widgets["new_text_field_widget"].font_color = (1, 0, 0)
+        obj.widgets["new_text_field_widget"].alignment = (
+            2  # TODO: why is alignment not rendered right in the appearance stream?
+        )
+        obj.widgets["new_checkbox_widget"].size = 40
+        obj.widgets["new_radio_group"].size = 50
 
         request.config.results["expected_path"] = expected_path
         request.config.results["stream"] = obj.read()
