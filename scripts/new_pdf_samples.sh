e97013e317460b1ec90494524db970839c1fb5aa
chinapandaman <benjaminli801@gmail.com>
2025-12-17 19:50:16 -0600

    PPF-1336: thanks gemini

--- scripts/new_pdf_samples.sh
+++ scripts/new_pdf_samples.sh
@@ -2,4 +2,4 @@ if [ "$VIRTUAL_ENV" == "" ] && [ "$PYPDFFORM_ENV" != "container" ]; then
   source "./venv/bin/activate"
 fi
 
-coverage run -m pytest --regenerate=1
+PYTHONPATH=. pytest --regenerate=1
