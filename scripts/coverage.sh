b4d3ce45be266aa635f21f0c4c4dfd69a8d9a0f5
chinapandaman <benjaminli801@gmail.com>
2025-12-30 20:16:16 -0600

    PPF-1355: use include instead of omit for coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,6 +8,8 @@ coverage run -m pytest && coverage html
 if [ "$PYPDFFORM_ENV" == "container" ]; then
   echo "Coverage report: http://localhost:8000/htmlcov/index.html"
   bash ./scripts/http_server.sh > /dev/null 2>&1
+elif [[ "$OSTYPE" == darwin* ]]; then
+  open -a "Google Chrome" htmlcov/index.html
 else
   google-chrome htmlcov/index.html
 fi
