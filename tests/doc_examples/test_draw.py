2a052fafda6eb4d38d8d1cd2432b45897d8d3734
chinapandaman <benjaminli801@gmail.com>
2026-01-01 21:14:57 -0600

    PPF-1376: add doc test

--- tests/doc_examples/test_draw.py
+++ tests/doc_examples/test_draw.py
@@ -225,3 +225,37 @@ def test_draw_circle(static_pdfs, pdf_samples, request):
 
         assert len(pdf.read()) == len(expected)
         assert pdf.read() == expected
+
+
+def test_draw_ellipse(static_pdfs, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_draw_ellipse.pdf")
+
+    ellipses = [
+        RawElements.RawEllipse(
+            page_number=1,
+            x1=100,
+            y1=100,
+            x2=250,
+            y2=200,
+        ),
+        RawElements.RawEllipse(
+            page_number=1,
+            x1=300,
+            y1=100,
+            x2=500,
+            y2=250,
+            color=(1, 0, 0),  # optional
+            fill_color=(0, 1, 0),  # optional
+        ),
+    ]
+
+    pdf = PdfWrapper(os.path.join(static_pdfs, "sample_template.pdf")).draw(ellipses)
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = pdf.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(pdf.read()) == len(expected)
+        assert pdf.read() == expected
