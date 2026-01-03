92786fd96bce71f3cab861351c1030c6bb9e918b
chinapandaman <benjaminli801@gmail.com>
2026-01-02 21:53:12 -0600

    PPF-1378: test checkbox

--- tests/test_js.py
+++ tests/test_js.py
@@ -71,3 +71,36 @@ def test_text_field_scripts(template_stream, pdf_samples, request):
 
         assert len(pdf.read()) == len(expected)
         assert pdf.read() == expected
+
+
+def test_checkbox_scripts(template_stream, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "js", "test_checkbox_scripts.pdf")
+    with open(expected_path, "rb+") as f:
+        pdf = PdfWrapper(template_stream)
+        pdf.widgets["check"].on_hovered_over_javascript = (
+            'this.getField("test").value = "hoverover";'
+        )
+        pdf.widgets["check"].on_hovered_off_javascript = (
+            'this.getField("test").value = "hoveroff";'
+        )
+        pdf.widgets["check"].on_mouse_pressed_javascript = (
+            'this.getField("test").value = "pressed";'
+        )
+        pdf.widgets["check"].on_mouse_released_javascript = (
+            'this.getField("test").value = "released";'
+        )
+
+        pdf.widgets["check_2"].on_focused_javascript = (
+            'this.getField("test_2").value = "focused";'
+        )
+        pdf.widgets["check_2"].off_focused_javascript = (
+            'this.getField("test_2").value = "defocused";'
+        )
+
+        request.config.results["expected_path"] = expected_path
+        request.config.results["stream"] = pdf.read()
+
+        expected = f.read()
+
+        assert len(pdf.read()) == len(expected)
+        assert pdf.read() == expected
