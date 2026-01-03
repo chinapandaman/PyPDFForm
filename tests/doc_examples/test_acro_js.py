02fa79de55e15fbd7a5de9fe6b4e07e21247fbfc
chinapandaman <benjaminli801@gmail.com>
2026-01-03 14:08:49 -0600

    PPF-1384: test mouse pressed

--- tests/doc_examples/test_acro_js.py
+++ tests/doc_examples/test_acro_js.py
@@ -55,12 +55,31 @@ def test_on_hover(static_pdfs, pdf_samples, js_samples, request):
         assert len(form.read()) == len(expected)
         assert form.read() == expected
 
+
 def test_off_hover(static_pdfs, pdf_samples, js_samples, request):
     expected_path = os.path.join(pdf_samples, "docs", "test_off_hover.pdf")
 
     form = PdfWrapper(os.path.join(static_pdfs, "sample_template.pdf"))
     form.widgets["test"].on_hovered_off_javascript = os.path.join(
         js_samples, "doc_examples", "test_off_hover.js"
+    )
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = form.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(form.read()) == len(expected)
+        assert form.read() == expected
+
+
+def test_mouse_pressed(static_pdfs, pdf_samples, js_samples, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_mouse_pressed.pdf")
+
+    form = PdfWrapper(os.path.join(static_pdfs, "sample_template.pdf"))
+    form.widgets["test"].on_mouse_pressed_javascript = os.path.join(
+        js_samples, "doc_examples", "test_mouse_pressed.js"
     )
 
     request.config.results["expected_path"] = expected_path
