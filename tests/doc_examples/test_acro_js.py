860d9a0399069812532b62a0fb0622a1de41297f
chinapandaman <benjaminli801@gmail.com>
2026-01-03 14:06:40 -0600

    PPF-1384: test off hover

--- tests/doc_examples/test_acro_js.py
+++ tests/doc_examples/test_acro_js.py
@@ -54,3 +54,20 @@ def test_on_hover(static_pdfs, pdf_samples, js_samples, request):
 
         assert len(form.read()) == len(expected)
         assert form.read() == expected
+
+def test_off_hover(static_pdfs, pdf_samples, js_samples, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_off_hover.pdf")
+
+    form = PdfWrapper(os.path.join(static_pdfs, "sample_template.pdf"))
+    form.widgets["test"].on_hovered_off_javascript = os.path.join(
+        js_samples, "doc_examples", "test_off_hover.js"
+    )
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = form.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(form.read()) == len(expected)
+        assert form.read() == expected
