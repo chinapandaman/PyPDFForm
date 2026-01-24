21364072aa1dd7d22ef2a499021b89bd531b4ff9
chinapandaman <benjaminli801@gmail.com>
2026-01-24 15:28:28 -0600

    PPF-1429: remove max length tests

--- tests/test_fill_max_length_text_field.py
+++ tests/test_fill_max_length_text_field.py
@@ -34,37 +34,6 @@ def test_fill_max_length_text_field_all_chars(
         assert obj.read() == expected
 
 
-def test_fill_max_length_text_field_all_chars_flatten(
-    sample_template_with_max_length_text_field, max_length_expected_directory, request
-):
-    expected_path = os.path.join(
-        max_length_expected_directory,
-        "test_fill_max_length_text_field_all_chars_flatten.pdf",
-    )
-    with open(
-        expected_path,
-        "rb+",
-    ) as f:
-        obj = PdfWrapper(sample_template_with_max_length_text_field).fill(
-            {
-                "FirstName": "John",
-                "MiddleName": "Joe",
-                "LastName": "XXXXXXXXXX",
-                "Awesomeness": True,
-                "Gender": 0,
-            },
-            flatten=True,
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
 def test_fill_max_length_text_field_odd_chars(
     sample_template_with_max_length_text_field, max_length_expected_directory, request
 ):
@@ -94,37 +63,6 @@ def test_fill_max_length_text_field_odd_chars(
         assert obj.read() == expected
 
 
-def test_fill_max_length_text_field_odd_chars_flatten(
-    sample_template_with_max_length_text_field, max_length_expected_directory, request
-):
-    expected_path = os.path.join(
-        max_length_expected_directory,
-        "test_fill_max_length_text_field_odd_chars_flatten.pdf",
-    )
-    with open(
-        expected_path,
-        "rb+",
-    ) as f:
-        obj = PdfWrapper(sample_template_with_max_length_text_field).fill(
-            {
-                "FirstName": "John",
-                "MiddleName": "Joe",
-                "LastName": "XXX",
-                "Awesomeness": True,
-                "Gender": 0,
-            },
-            flatten=True,
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
 def test_fill_max_length_text_field_even_chars(
     sample_template_with_max_length_text_field, max_length_expected_directory, request
 ):
@@ -143,37 +81,6 @@ def test_fill_max_length_text_field_even_chars(
                 "Awesomeness": True,
                 "Gender": 0,
             }
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-def test_fill_max_length_text_field_even_chars_flatten(
-    sample_template_with_max_length_text_field, max_length_expected_directory, request
-):
-    expected_path = os.path.join(
-        max_length_expected_directory,
-        "test_fill_max_length_text_field_even_chars_flatten.pdf",
-    )
-    with open(
-        expected_path,
-        "rb+",
-    ) as f:
-        obj = PdfWrapper(sample_template_with_max_length_text_field).fill(
-            {
-                "FirstName": "John",
-                "MiddleName": "Joe",
-                "LastName": "XXXX",
-                "Awesomeness": True,
-                "Gender": 0,
-            },
-            flatten=True,
         )
 
         request.config.results["expected_path"] = expected_path
@@ -273,36 +180,6 @@ def test_fill_comb_text_field_odd_chars(
         assert obj.read() == expected
 
 
-def test_fill_comb_text_field_odd_chars_flatten(
-    sample_template_with_comb_text_field, max_length_expected_directory, request
-):
-    expected_path = os.path.join(
-        max_length_expected_directory, "test_fill_comb_text_field_odd_chars_flatten.pdf"
-    )
-    with open(
-        expected_path,
-        "rb+",
-    ) as f:
-        obj = PdfWrapper(sample_template_with_comb_text_field).fill(
-            {
-                "FirstName": "John",
-                "MiddleName": "Joe",
-                "LastName": "XXX",
-                "Awesomeness": True,
-                "Gender": 0,
-            },
-            flatten=True,
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
 def test_fill_comb_text_field_even_chars(
     sample_template_with_comb_text_field, max_length_expected_directory, request
 ):
@@ -321,37 +198,6 @@ def test_fill_comb_text_field_even_chars(
                 "Awesomeness": True,
                 "Gender": 0,
             }
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-def test_fill_comb_text_field_even_chars_flatten(
-    sample_template_with_comb_text_field, max_length_expected_directory, request
-):
-    expected_path = os.path.join(
-        max_length_expected_directory,
-        "test_fill_comb_text_field_even_chars_flatten.pdf",
-    )
-    with open(
-        expected_path,
-        "rb+",
-    ) as f:
-        obj = PdfWrapper(sample_template_with_comb_text_field).fill(
-            {
-                "FirstName": "John",
-                "MiddleName": "Joe",
-                "LastName": "XXXXXX",
-                "Awesomeness": True,
-                "Gender": 0,
-            },
-            flatten=True,
         )
 
         request.config.results["expected_path"] = expected_path
@@ -402,39 +248,6 @@ def test_fill_comb_text_field_even_chars_right_aligned(
                 "Awesomeness": True,
                 "Gender": 0,
             }
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-def test_fill_comb_text_field_even_chars_right_aligned_flatten(
-    sample_template_with_comb_text_field_right_aligned,
-    max_length_expected_directory,
-    request,
-):
-    expected_path = os.path.join(
-        max_length_expected_directory,
-        "test_fill_comb_text_field_even_chars_right_aligned_flatten.pdf",
-    )
-    with open(
-        expected_path,
-        "rb+",
-    ) as f:
-        obj = PdfWrapper(sample_template_with_comb_text_field_right_aligned).fill(
-            {
-                "FirstName": "John",
-                "MiddleName": "Joe",
-                "LastName": "XXXXXX",
-                "Awesomeness": True,
-                "Gender": 0,
-            },
-            flatten=True,
         )
 
         request.config.results["expected_path"] = expected_path
