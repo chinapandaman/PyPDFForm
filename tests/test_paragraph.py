2d52686f275ee259386f6b1fdbbcdfc8a103d354
chinapandaman <benjaminli801@gmail.com>
2026-01-24 15:42:06 -0600

    PPF-1429: remove paragraph tests

--- tests/test_paragraph.py
+++ tests/test_paragraph.py
@@ -25,27 +25,6 @@ def test_paragraph_y_coordinate(sample_template_with_paragraph, pdf_samples, req
         assert obj.read() == expected
 
 
-def test_paragraph_y_coordinate_flatten(
-    sample_template_with_paragraph, pdf_samples, request
-):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_y_coordinate_flatten.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_with_paragraph).fill(
-            {"paragraph_1": "test paragraph"},
-            flatten=True,
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
 def test_paragraph_auto_wrap(sample_template_with_paragraph, pdf_samples, request):
     expected_path = os.path.join(
         pdf_samples, "paragraph", "test_paragraph_auto_wrap.pdf"
@@ -55,29 +34,6 @@ def test_paragraph_auto_wrap(sample_template_with_paragraph, pdf_samples, reques
             {
                 "paragraph_1": "t xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx t"
             }
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-def test_paragraph_auto_wrap_flatten(
-    sample_template_with_paragraph, pdf_samples, request
-):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_auto_wrap_flatten.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_with_paragraph).fill(
-            {
-                "paragraph_1": "t xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx t"
-            },
-            flatten=True,
         )
 
         request.config.results["expected_path"] = expected_path
@@ -111,27 +67,6 @@ def test_paragraph_auto_font(
 
 
 @pytest.mark.posix_only
-def test_paragraph_auto_font_flatten(
-    sample_template_with_paragraph_auto_font, pdf_samples, request
-):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_auto_font_flatten.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_with_paragraph_auto_font).fill(
-            {"paragraph": "test paragraph"}, flatten=True
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-@pytest.mark.posix_only
 def test_paragraph_auto_font_auto_wrap(
     sample_template_with_paragraph_auto_font, pdf_samples, request
 ):
@@ -143,30 +78,6 @@ def test_paragraph_auto_font_auto_wrap(
             {
                 "paragraph": "t xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx t"
             }
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-@pytest.mark.posix_only
-def test_paragraph_auto_font_auto_wrap_flatten(
-    sample_template_with_paragraph_auto_font, pdf_samples, request
-):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_auto_font_auto_wrap_flatten.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_with_paragraph_auto_font).fill(
-            {
-                "paragraph": "t xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx t"
-            },
-            flatten=True,
         )
 
         request.config.results["expected_path"] = expected_path
@@ -286,36 +197,6 @@ def test_sejda_complex_paragraph_multiple_line_alignment(
         assert obj.read() == expected
 
 
-def test_sejda_complex_paragraph_multiple_line_alignment_flatten(
-    sejda_template_complex, pdf_samples, request
-):
-    expected_path = os.path.join(
-        pdf_samples,
-        "paragraph",
-        "test_sejda_complex_paragraph_multiple_line_alignment_flatten.pdf",
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sejda_template_complex).fill(
-            {
-                "paragraph_font_auto_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_auto_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_auto_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_ten_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_ten_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_ten_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            },
-            flatten=True,
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
 @pytest.mark.posix_only
 def test_paragraph_complex(sample_template_paragraph_complex, pdf_samples, request):
     expected_path = os.path.join(pdf_samples, "paragraph", "test_paragraph_complex.pdf")
@@ -341,35 +222,6 @@ def test_paragraph_complex(sample_template_paragraph_complex, pdf_samples, reque
 
 
 @pytest.mark.posix_only
-def test_paragraph_complex_flatten(
-    sample_template_paragraph_complex, pdf_samples, request
-):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_complex_flatten.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_paragraph_complex).fill(
-            {
-                "paragraph_font_auto_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_auto_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_auto_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_ten_left": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_ten_right": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-                "paragraph_font_ten_center": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            },
-            flatten=True,
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-@pytest.mark.posix_only
 def test_paragraph_max_length(
     sample_template_with_paragraph_max_length, pdf_samples, request
 ):
@@ -381,30 +233,6 @@ def test_paragraph_max_length(
             {
                 "paragraph": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
             }
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-@pytest.mark.posix_only
-def test_paragraph_max_length_flatten(
-    sample_template_with_paragraph_max_length, pdf_samples, request
-):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_max_length_flatten.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_with_paragraph_max_length).fill(
-            {
-                "paragraph": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            },
-            flatten=True,
         )
 
         request.config.results["expected_path"] = expected_path
