962c0cc03176af1da0de2abc4494bc089507faeb
chinapandaman <benjaminli801@gmail.com>
2026-01-24 16:10:06 -0600

    PPF-1433: first batch

--- tests/test_paragraph.py
+++ tests/test_paragraph.py
@@ -7,44 +7,6 @@ import pytest
 from PyPDFForm import PdfWrapper
 
 
-def test_paragraph_y_coordinate(sample_template_with_paragraph, pdf_samples, request):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_y_coordinate.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_with_paragraph).fill(
-            {"paragraph_1": "test paragraph"}
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-def test_paragraph_auto_wrap(sample_template_with_paragraph, pdf_samples, request):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_auto_wrap.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_with_paragraph).fill(
-            {
-                "paragraph_1": "t xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx t"
-            }
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
 @pytest.mark.posix_only
 def test_paragraph_auto_font(
     sample_template_with_paragraph_auto_font, pdf_samples, request
