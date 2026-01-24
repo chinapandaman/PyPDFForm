6c0fab621e9a1681f5f7435620fd77b5ab265b4e
chinapandaman <benjaminli801@gmail.com>
2026-01-24 16:11:57 -0600

    PPF-1433: second batch

--- tests/test_paragraph.py
+++ tests/test_paragraph.py
@@ -7,50 +7,6 @@ import pytest
 from PyPDFForm import PdfWrapper
 
 
-@pytest.mark.posix_only
-def test_paragraph_auto_font(
-    sample_template_with_paragraph_auto_font, pdf_samples, request
-):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_auto_font.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_with_paragraph_auto_font).fill(
-            {"paragraph": "test paragraph"}
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
-@pytest.mark.posix_only
-def test_paragraph_auto_font_auto_wrap(
-    sample_template_with_paragraph_auto_font, pdf_samples, request
-):
-    expected_path = os.path.join(
-        pdf_samples, "paragraph", "test_paragraph_auto_font_auto_wrap.pdf"
-    )
-    with open(expected_path, "rb+") as f:
-        obj = PdfWrapper(sample_template_with_paragraph_auto_font).fill(
-            {
-                "paragraph": "t xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx t"
-            }
-        )
-
-        request.config.results["expected_path"] = expected_path
-        request.config.results["stream"] = obj.read()
-
-        expected = f.read()
-
-        assert len(obj.read()) == len(expected)
-        assert obj.read() == expected
-
-
 def test_fill_sejda_complex(sejda_template_complex, pdf_samples, request):
     expected_path = os.path.join(
         pdf_samples, "paragraph", "test_fill_sejda_complex.pdf"
