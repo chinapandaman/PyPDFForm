f13373431332eb73197665f12e72293d7843a55f
chinapandaman <benjaminli801@gmail.com>
2026-02-07 07:42:31 -0600

    PPF-1498: move xml coverage to test

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@ if [ "$VIRTUAL_ENV" == "" ] && [ "$PYPDFFORM_ENV" != "container" ]; then
   source "./venv/bin/activate"
 fi
 
-coverage run -m pytest && coverage report --fail-under=100
+coverage run -m pytest && coverage xml && coverage report --fail-under=100
