4616fd6fb27d8a94dbccb607088f337b79a0723a
chinapandaman <benjaminli801@gmail.com>
2026-02-10 20:45:35 -0600

    PPF-1542: add doc tests

--- tests/doc_examples/test_annotation.py
+++ tests/doc_examples/test_annotation.py
@@ -38,3 +38,87 @@ def test_text_annotations(static_pdfs, pdf_samples, request):
 
         assert len(pdf.read()) == len(expected)
         assert pdf.read() == expected
+
+
+def test_highlight_annotations(static_pdfs, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_highlight_annotations.pdf")
+
+    pdf = PdfWrapper(os.path.join(static_pdfs, "sample_template.pdf")).annotate(
+        [
+            Annotations.HighlightAnnotation(
+                page_number=1, x=70, y=705, width=95, height=20
+            )
+        ]
+    )
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = pdf.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(pdf.read()) == len(expected)
+        assert pdf.read() == expected
+
+
+def test_underline_annotations(static_pdfs, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_underline_annotations.pdf")
+
+    pdf = PdfWrapper(os.path.join(static_pdfs, "sample_template.pdf")).annotate(
+        [
+            Annotations.UnderlineAnnotation(
+                page_number=1, x=70, y=705, width=95, height=20
+            )
+        ]
+    )
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = pdf.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(pdf.read()) == len(expected)
+        assert pdf.read() == expected
+
+
+def test_squiggly_annotations(static_pdfs, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_squiggly_annotations.pdf")
+
+    pdf = PdfWrapper(os.path.join(static_pdfs, "sample_template.pdf")).annotate(
+        [
+            Annotations.SquigglyAnnotation(
+                page_number=1, x=70, y=705, width=95, height=20
+            )
+        ]
+    )
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = pdf.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(pdf.read()) == len(expected)
+        assert pdf.read() == expected
+
+
+def test_strikeout_annotations(static_pdfs, pdf_samples, request):
+    expected_path = os.path.join(pdf_samples, "docs", "test_strikeout_annotations.pdf")
+
+    pdf = PdfWrapper(os.path.join(static_pdfs, "sample_template.pdf")).annotate(
+        [
+            Annotations.StrikeOutAnnotation(
+                page_number=1, x=70, y=705, width=95, height=20
+            )
+        ]
+    )
+
+    request.config.results["expected_path"] = expected_path
+    request.config.results["stream"] = pdf.read()
+
+    with open(expected_path, "rb+") as f:
+        expected = f.read()
+
+        assert len(pdf.read()) == len(expected)
+        assert pdf.read() == expected
