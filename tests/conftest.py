afb43a9345539bd09af028f47ee14aa89f216a17
chinapandaman <benjaminli801@gmail.com>
2026-03-28 11:26:37 -0500

    PPF-1632: downgrade sejda support level

--- tests/conftest.py
+++ tests/conftest.py
@@ -84,12 +84,6 @@ def data_dict():
 
 
 @pytest.fixture
-def sejda_template(pdf_samples):
-    with open(os.path.join(pdf_samples, "sample_template_sejda.pdf"), "rb+") as f:
-        return f.read()
-
-
-@pytest.fixture
 def sejda_template_complex(pdf_samples):
     with open(
         os.path.join(pdf_samples, "paragraph", "sample_template_sejda_complex.pdf"),
@@ -212,39 +206,6 @@ def dropdown_alignment_sejda(pdf_samples):
         "rb+",
     ) as f:
         return f.read()
-
-
-@pytest.fixture
-def sejda_data():
-    return {
-        "date": "01-01",
-        "year": "21",
-        "buyer_name": "John Doe",
-        "buyer_address": "1 N Main St, Chicago, IL 60000",
-        "seller_name": "Jack Smith",
-        "seller_address": "2 S Main St, Chicago, IL 60000",
-        "make": "AK",
-        "model": "47",
-        "caliber": "7.62-x39mm",
-        "serial_number": "111111",
-        "purchase_option": 0,
-        "date_of_this_bill": True,
-        "at_future_date": True,
-        "other": True,
-        "other_reason": "NO REASONS",
-        "payment_amount": "400",
-        "future_date": "01-01",
-        "future_year": "22",
-        "exchange_for": "Food",
-        "buyer_name_printed": "John Doe",
-        "seller_name_printed": "Jack Smith",
-        "buyer_signed_date": "2021-01-01",
-        "seller_signed_date": "2021-01-01",
-        "buyer_dl_number": "D000-4609-0001",
-        "seller_dl_number": "S530-4209-0001",
-        "buyer_dl_state": "IL",
-        "seller_dl_state": "IL",
-    }
 
 
 @pytest.fixture
