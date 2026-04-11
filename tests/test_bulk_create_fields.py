38fccd7aef62661c140766f6bb32ca5b71d20c7d
chinapandaman <benjaminli801@gmail.com>
2026-04-10 21:52:45 -0500

    PPF-1679: fix test cognitive complexity

--- tests/test_bulk_create_fields.py
+++ tests/test_bulk_create_fields.py
@@ -62,19 +62,20 @@ def test_bulk_create_fields_stress_max(pdf_samples, request):
             obj += PdfWrapper(os.path.join(pdf_samples, "dummy.pdf"))
         obj.bulk_create_fields(fields)
 
+        prefix_to_page = {
+            "text_": 1,
+            "check_": 2,
+            "dropdown_": 3,
+            "radio_": 4,
+            "image_": 5,
+            "signature_": 6,
+        }
+
         for k, v in obj.widgets.items():
-            if k.startswith("text_"):
-                assert v.page_number == 1
-            if k.startswith("check_"):
-                assert v.page_number == 2
-            if k.startswith("dropdown_"):
-                assert v.page_number == 3
-            if k.startswith("radio_"):
-                assert v.page_number == 4
-            if k.startswith("image_"):
-                assert v.page_number == 5
-            if k.startswith("signature_"):
-                assert v.page_number == 6
+            for each, page in prefix_to_page.items():
+                if k.startswith(each):
+                    assert v.page_number == page
+                    break
 
         request.config.results["expected_path"] = expected_path
         request.config.results["stream"] = obj.read()
