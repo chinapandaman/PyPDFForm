bbaac5de65a333e7fd1c69c4261d1625c7ab1ee3
chinapandaman <benjaminli801@gmail.com>
2026-04-12 20:17:48 -0500

    PPF-1695: add image test

--- tests/doc_examples/cli/test_cli_create.py
+++ tests/doc_examples/cli/test_cli_create.py
@@ -176,9 +176,7 @@ def test_create_dropdown_with_export_values(pdf_samples, json_samples, tmp_path)
 
 @pytest.mark.cli_test
 def test_create_sig(pdf_samples, json_samples, tmp_path):
-    expected_path = os.path.join(
-        pdf_samples, "docs", "test_create_sig.pdf"
-    )
+    expected_path = os.path.join(pdf_samples, "docs", "test_create_sig.pdf")
     output_path = os.path.join(tmp_path, "output.pdf")
 
     result = runner.invoke(
@@ -189,6 +187,33 @@ def test_create_sig(pdf_samples, json_samples, tmp_path):
             os.path.join(pdf_samples, "dummy.pdf"),
             "-f",
             os.path.join(json_samples, "test_create_sig.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_create_image(pdf_samples, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_create_image.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "create",
+            "field",
+            os.path.join(pdf_samples, "dummy.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_create_image.json"),
             "-o",
             output_path,
         ],
