7b66d550fe53e13b7c96c10d119c01ea86dd3e30
chinapandaman <benjaminli801@gmail.com>
2026-04-12 19:47:25 -0500

    PPF-1695: start adding tests

---
+++ tests/doc_examples/cli/test_cli_create.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from typer.testing import CliRunner
+
+from PyPDFForm.cli import cli_app
+
+runner = CliRunner()
+
+
+def test_bulk_create_fields(pdf_samples, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_bulk_create_fields.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "create",
+            "field",
+            os.path.join(pdf_samples, "dummy.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_bulk_create_fields.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
