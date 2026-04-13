2aee1573cb408a5e3c304a20558780e429ca7c4e
chinapandaman <benjaminli801@gmail.com>
2026-04-12 20:13:18 -0500

    PPF-1695: add dropdown test

--- tests/doc_examples/cli/test_cli_create.py
+++ tests/doc_examples/cli/test_cli_create.py
@@ -116,3 +116,30 @@ def test_create_radio(pdf_samples, json_samples, tmp_path):
 
         assert len(expected) == len(actual)
         assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_create_dropdown(pdf_samples, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_create_dropdown.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "create",
+            "field",
+            os.path.join(pdf_samples, "dummy.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_create_dropdown.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
