fddaa27491b6a01015dd92e10f8627b5ccc758a5
chinapandaman <benjaminli801@gmail.com>
2026-04-12 20:54:25 -0500

    PPF-1697: add radio test

--- tests/doc_examples/cli/test_cli_fill.py
+++ tests/doc_examples/cli/test_cli_fill.py
@@ -11,7 +11,7 @@ runner = CliRunner()
 
 
 @pytest.mark.cli_test
-def test_fill_text_check(pdf_samples, json_samples, tmp_path):
+def test_fill_text_check(pdf_samples, static_pdfs, json_samples, tmp_path):
     expected_path = os.path.join(pdf_samples, "docs", "test_fill_text_check.pdf")
     output_path = os.path.join(tmp_path, "output.pdf")
 
@@ -19,9 +19,35 @@ def test_fill_text_check(pdf_samples, json_samples, tmp_path):
         cli_app,
         [
             "fill",
-            os.path.join(pdf_samples, "sample_template.pdf"),
+            os.path.join(static_pdfs, "sample_template.pdf"),
             "-f",
             os.path.join(json_samples, "test_fill_text_check.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_fill_radio(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_fill_radio.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "fill",
+            os.path.join(static_pdfs, "sample_template_with_radio_button.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_fill_radio.json"),
             "-o",
             output_path,
         ],
