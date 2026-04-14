d3d6bd0e1b5bb2a344e3c5de8663c7e55bfd4fea
chinapandaman <benjaminli801@gmail.com>
2026-04-13 19:57:41 -0500

    PPF-1707: add test for draw line

--- tests/doc_examples/cli/test_cli_draw.py
+++ tests/doc_examples/cli/test_cli_draw.py
@@ -63,3 +63,30 @@ def test_draw_image(pdf_samples, static_pdfs, json_samples, tmp_path):
 
         assert len(expected) == len(actual)
         assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_draw_line(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_draw_line.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "create",
+            "raw",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_draw_line.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
