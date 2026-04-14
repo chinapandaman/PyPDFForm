a972fe3a6a1fd30aacdeb28a52aa53e3b274d587
chinapandaman <benjaminli801@gmail.com>
2026-04-14 18:47:03 -0500

    PPF-1718: also update doc test

--- tests/doc_examples/cli/test_cli_coordinate.py
+++ tests/doc_examples/cli/test_cli_coordinate.py
@@ -57,6 +57,7 @@ def test_field_coordinates_dimensions(static_pdfs):
 
     wrapper = PdfWrapper(expected_path)
     obj = json.loads(result.output)
+    assert obj["page_number"] == wrapper.widgets["test"].page_number
     assert obj["x"] == wrapper.widgets["test"].x
     assert obj["y"] == wrapper.widgets["test"].y
     assert obj["width"] == wrapper.widgets["test"].width
