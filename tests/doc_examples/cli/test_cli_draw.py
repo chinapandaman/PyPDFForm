c7d85bc9078643087801add8f9ec750658799c5f
chinapandaman <benjaminli801@gmail.com>
2026-04-13 20:01:55 -0500

    PPF-1707: add test for draw ellipse

--- tests/doc_examples/cli/test_cli_draw.py
+++ tests/doc_examples/cli/test_cli_draw.py
@@ -144,3 +144,30 @@ def test_draw_circle(pdf_samples, static_pdfs, json_samples, tmp_path):
 
         assert len(expected) == len(actual)
         assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_draw_ellipse(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_draw_ellipse.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "create",
+            "raw",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_draw_ellipse.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
