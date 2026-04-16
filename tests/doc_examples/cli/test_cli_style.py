6986286ebca6d84afd7c7bc23b0ea885f78dd45c
chinapandaman <benjaminli801@gmail.com>
2026-04-15 20:59:33 -0500

    PPF-1727: add font size test

--- tests/doc_examples/cli/test_cli_style.py
+++ tests/doc_examples/cli/test_cli_style.py
@@ -12,7 +12,7 @@ runner = CliRunner()
 
 
 @pytest.mark.cli_test
-def test_change_text_font(pdf_samples, static_pdfs, json_samples, data_dict, tmp_path):
+def test_change_text_font(pdf_samples, static_pdfs, json_samples, tmp_path):
     expected_path = os.path.join(pdf_samples, "docs", "test_change_text_font.pdf")
     output_path = os.path.join(tmp_path, "output.pdf")
 
@@ -24,6 +24,43 @@ def test_change_text_font(pdf_samples, static_pdfs, json_samples, data_dict, tmp
             os.path.join(static_pdfs, "sample_template.pdf"),
             "-f",
             os.path.join(json_samples, "test_change_text_font.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    runner.invoke(
+        cli_app,
+        [
+            "fill",
+            output_path,
+            "-f",
+            os.path.join(json_samples, "test_fill_text_check.json"),
+        ],
+    )
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_change_text_font_size(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_change_text_font_size.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "update",
+            "field",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_change_text_font_size.json"),
             "-o",
             output_path,
         ],
