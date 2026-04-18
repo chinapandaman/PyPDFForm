bb48aa0f0446b27738175beac79a77e9031fbcfb
chinapandaman <benjaminli801@gmail.com>
2026-04-18 08:12:31 -0500

    PPF-1729: gemini usable this morning

---
+++ tests/doc_examples/cli/test_cli_acro_js.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import pytest
+from typer.testing import CliRunner
+
+from PyPDFForm.cli import cli_app
+
+runner = CliRunner()
+
+
+@pytest.mark.cli_test
+def test_on_hover(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_on_hover.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "update",
+            "field",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_on_hover.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_off_hover(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_off_hover.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "update",
+            "field",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_off_hover.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_mouse_pressed(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_mouse_pressed.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "update",
+            "field",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_mouse_pressed.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_mouse_released(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_mouse_released.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "update",
+            "field",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_mouse_released.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_on_focused(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_on_focused.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "update",
+            "field",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_on_focused.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.cli_test
+def test_off_focused(pdf_samples, static_pdfs, json_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_off_focused.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    result = runner.invoke(
+        cli_app,
+        [
+            "update",
+            "field",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "-f",
+            os.path.join(json_samples, "test_off_focused.json"),
+            "-o",
+            output_path,
+        ],
+    )
+    assert result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
