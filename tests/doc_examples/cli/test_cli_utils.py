a104032a4751a37ce9faa1adc310705cac027528
chinapandaman <benjaminli801@gmail.com>
2026-04-18 20:56:33 -0500

    PPF-1739: add reorg pages test

--- tests/doc_examples/cli/test_cli_utils.py
+++ tests/doc_examples/cli/test_cli_utils.py
@@ -153,6 +153,65 @@ def test_merge(static_pdfs, pdf_samples, tmp_path):
 
 
 @pytest.mark.cli_test
+def test_reorg_pages(static_pdfs, pdf_samples, tmp_path):
+    expected_path = os.path.join(pdf_samples, "docs", "test_reorg_pages.pdf")
+    first_page_path = os.path.join(tmp_path, "first_page.pdf")
+    remaining_pages_path = os.path.join(tmp_path, "remaining_pages.pdf")
+    output_path = os.path.join(tmp_path, "output.pdf")
+
+    first_page_result = runner.invoke(
+        cli_app,
+        [
+            "create",
+            "pages",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "--start",
+            "1",
+            "--end",
+            "1",
+            "-o",
+            first_page_path,
+        ],
+    )
+    assert first_page_result.exit_code == 0
+
+    remaining_pages_result = runner.invoke(
+        cli_app,
+        [
+            "create",
+            "pages",
+            os.path.join(static_pdfs, "sample_template.pdf"),
+            "--start",
+            "2",
+            "-o",
+            remaining_pages_path,
+        ],
+    )
+    assert remaining_pages_result.exit_code == 0
+
+    combine_result = runner.invoke(
+        cli_app,
+        [
+            "create",
+            "combine",
+            first_page_path,
+            os.path.join(pdf_samples, "dummy.pdf"),
+            remaining_pages_path,
+            "-o",
+            output_path,
+        ],
+    )
+    assert combine_result.exit_code == 0
+
+    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
+        expected = f1.read()
+        actual = f2.read()
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.cli_test
 def test_change_version(static_pdfs, tmp_path):
     output_path = os.path.join(tmp_path, "output.pdf")
 
