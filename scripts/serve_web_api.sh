8ee82a5dc7bb656342021ca4ba60ada80a6fb3ca
chinapandaman <benjaminli801@gmail.com>
2026-04-29 19:39:41 -0500

    PPF-1791: fix host ip

--- scripts/serve_web_api.sh
+++ scripts/serve_web_api.sh
@@ -2,4 +2,4 @@ if [ "$VIRTUAL_ENV" == "" ] && [ "$PYPDFFORM_ENV" != "container" ]; then
   source "./venv/bin/activate"
 fi
 
-fastapi dev PyPDFForm/api/root.py --port 8080
+fastapi dev PyPDFForm/api/root.py --host 0.0.0.0 --port 8080
