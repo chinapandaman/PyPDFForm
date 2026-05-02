ba2aadf06b9b0d23d705ccb2e88b852ec656b3ee
chinapandaman <benjaminli801@gmail.com>
2026-05-01 22:08:41 -0500

    PPF-1793: test inspect data

--- tests/doc_examples/web_api/test_web_api_inspect.py
+++ tests/doc_examples/web_api/test_web_api_inspect.py
@@ -36,3 +36,26 @@ def test_schema(static_pdfs):
         },
         "additionalProperties": True,
     }
+
+
+@pytest.mark.web_api_test
+def test_data(static_pdfs):
+    path = os.path.join(static_pdfs, "sample_template_filled.pdf")
+    with open(path, "rb") as f:
+        response = client.post(
+            "/inspect/data",
+            files={
+                "pdf": ("sample_template_filled.pdf", f, "application/pdf"),
+            },
+        )
+
+    assert response.status_code == 200
+
+    assert json.loads(response.content) == {
+        "check": True,
+        "check_2": True,
+        "check_3": True,
+        "test": "test",
+        "test_2": "test2",
+        "test_3": "test3",
+    }
