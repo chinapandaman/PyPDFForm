ab6c22ba65573e079f087bae0e80fd384a8342d0
chinapandaman <benjaminli801@gmail.com>
2026-05-13 20:45:55 -0500

    PPF-1706: rename and move tests around

--- tests/doc_examples/cli/test_cli_install.py
+++ tests/doc_examples/cli/test_cli_install.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 import pytest
-from pypdf import PdfReader, PdfWriter
 from typer.testing import CliRunner
 
 from PyPDFForm import __version__
 from PyPDFForm.cli.root import cli_app
-from PyPDFForm.lib.constants import AcroForm, Title
 
 runner = CliRunner()
 
@@ -23,91 +19,3 @@ def test_get_version():
 
     assert long.output == f"v{__version__}\n"
     assert long.output == short.output
-
-
-@pytest.mark.cli_test
-def test_need_appearances_option(static_pdfs, tmp_path):
-    output_path = os.path.join(tmp_path, "output.pdf")
-    result = runner.invoke(
-        cli_app,
-        [
-            "--need-appearances",
-            "update",
-            "title",
-            os.path.join(static_pdfs, "sample_template.pdf"),
-            "-t",
-            "My PDF",
-            "-o",
-            output_path,
-        ],
-    )
-    assert result.exit_code == 0
-
-    reader = PdfReader(output_path)
-    assert reader.root_object[AcroForm]["/NeedAppearances"]
-
-
-@pytest.mark.cli_test
-def test_generate_appearance_streams_option(static_pdfs, tmp_path):
-    output_path = os.path.join(tmp_path, "output.pdf")
-    result = runner.invoke(
-        cli_app,
-        [
-            "--generate-appearance-streams",
-            "update",
-            "title",
-            os.path.join(static_pdfs, "sample_template.pdf"),
-            "-t",
-            "My PDF",
-            "-o",
-            output_path,
-        ],
-    )
-    assert result.exit_code == 0
-
-    reader = PdfReader(output_path)
-    assert "/NeedAppearances" not in reader.root_object[AcroForm]
-
-
-@pytest.mark.cli_test
-def test_preserve_metadata_option(static_pdfs, tmp_path):
-    with_metadata = os.path.join(tmp_path, "metadata.pdf")
-    output_path = os.path.join(tmp_path, "output.pdf")
-    writer = PdfWriter(os.path.join(static_pdfs, "sample_template.pdf"))
-    writer.add_metadata({"/foo": "bar"})
-    writer.write(with_metadata)
-
-    result = runner.invoke(
-        cli_app,
-        [
-            "--preserve-metadata",
-            "update",
-            "title",
-            with_metadata,
-            "-t",
-            "My PDF",
-            "-o",
-            output_path,
-        ],
-    )
-    assert result.exit_code == 0
-
-    reader = PdfReader(output_path)
-    assert (reader.metadata or {}).get(Title) == "My PDF"
-    assert (reader.metadata or {}).get("/foo") == "bar"
-
-
-@pytest.mark.cli_test
-def test_use_full_widget_name_option(static_pdfs):
-    result = runner.invoke(
-        cli_app,
-        [
-            "--use-full-widget-name",
-            "inspect",
-            "location",
-            os.path.join(static_pdfs, "sample_template_with_full_key.pdf"),
-            "--field",
-            "Gain de 2 classes.0",
-        ],
-    )
-    assert result.exit_code == 0
