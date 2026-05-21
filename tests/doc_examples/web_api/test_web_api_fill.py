bdc26e9c053709322e75d340058f090b715bc673
chinapandaman <benjaminli801@gmail.com>
2026-05-20 21:37:12 -0500

    PPF-1832: test sig

--- tests/doc_examples/web_api/test_web_api_fill.py
+++ tests/doc_examples/web_api/test_web_api_fill.py
@@ -105,3 +105,53 @@ def test_fill_dropdown_via_str(pdf_samples, static_pdfs, json_samples):
 
         assert len(expected) == len(actual)
         assert expected == actual
+
+
+@pytest.mark.posix_only
+@pytest.mark.web_api_test
+def test_fill_sig(pdf_samples, static_pdfs, json_samples):
+    expected_path = os.path.join(pdf_samples, "docs", "test_fill_sig.pdf")
+    path = os.path.join(static_pdfs, "sample_template_with_signature.pdf")
+    with (
+        open(path, "rb") as f,
+        open(os.path.join(json_samples, "test_fill_sig.json"), "r") as j,
+    ):
+        result = client.post(
+            "/fill",
+            data={"data": json.dumps(json.load(j))},
+            files={
+                "pdf": ("output.pdf", f, "application/pdf"),
+            },
+        )
+
+    with open(expected_path, "rb") as f:
+        expected = f.read()
+        actual = result.content
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.posix_only
+@pytest.mark.web_api_test
+def test_fill_sig_ratio(pdf_samples, static_pdfs, json_samples):
+    expected_path = os.path.join(pdf_samples, "docs", "test_fill_sig_ratio.pdf")
+    path = os.path.join(static_pdfs, "sample_template_with_signature.pdf")
+    with (
+        open(path, "rb") as f,
+        open(os.path.join(json_samples, "test_fill_sig_ratio.json"), "r") as j,
+    ):
+        result = client.post(
+            "/fill",
+            data={"data": json.dumps(json.load(j))},
+            files={
+                "pdf": ("output.pdf", f, "application/pdf"),
+            },
+        )
+
+    with open(expected_path, "rb") as f:
+        expected = f.read()
+        actual = result.content
+
+        assert len(expected) == len(actual)
+        assert expected == actual
