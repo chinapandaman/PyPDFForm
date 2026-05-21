6b1e90bc5b58b8b2f67233dccc1bd938b09012f1
chinapandaman <benjaminli801@gmail.com>
2026-05-20 21:35:51 -0500

    PPF-1832: test dropdown

--- tests/doc_examples/web_api/test_web_api_fill.py
+++ tests/doc_examples/web_api/test_web_api_fill.py
@@ -57,3 +57,51 @@ def test_fill_radio(pdf_samples, static_pdfs, json_samples):
 
         assert len(expected) == len(actual)
         assert expected == actual
+
+
+@pytest.mark.web_api_test
+def test_fill_dropdown(pdf_samples, static_pdfs, json_samples):
+    expected_path = os.path.join(pdf_samples, "docs", "test_fill_dropdown.pdf")
+    path = os.path.join(static_pdfs, "sample_template_with_dropdown.pdf")
+    with (
+        open(path, "rb") as f,
+        open(os.path.join(json_samples, "test_fill_dropdown.json"), "r") as j,
+    ):
+        result = client.post(
+            "/fill",
+            data={"data": json.dumps(json.load(j))},
+            files={
+                "pdf": ("output.pdf", f, "application/pdf"),
+            },
+        )
+
+    with open(expected_path, "rb") as f:
+        expected = f.read()
+        actual = result.content
+
+        assert len(expected) == len(actual)
+        assert expected == actual
+
+
+@pytest.mark.web_api_test
+def test_fill_dropdown_via_str(pdf_samples, static_pdfs, json_samples):
+    expected_path = os.path.join(pdf_samples, "docs", "test_fill_dropdown.pdf")
+    path = os.path.join(static_pdfs, "sample_template_with_dropdown.pdf")
+    with (
+        open(path, "rb") as f,
+        open(os.path.join(json_samples, "test_fill_dropdown_via_str.json"), "r") as j,
+    ):
+        result = client.post(
+            "/fill",
+            data={"data": json.dumps(json.load(j))},
+            files={
+                "pdf": ("output.pdf", f, "application/pdf"),
+            },
+        )
+
+    with open(expected_path, "rb") as f:
+        expected = f.read()
+        actual = result.content
+
+        assert len(expected) == len(actual)
+        assert expected == actual
