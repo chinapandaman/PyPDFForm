8032683da8db6238f5a574f65fed93ee575e93ed
chinapandaman <benjaminli801@gmail.com>
2026-05-20 21:34:27 -0500

    PPF-1832: test radio

--- tests/doc_examples/web_api/test_web_api_fill.py
+++ tests/doc_examples/web_api/test_web_api_fill.py
@@ -33,3 +33,27 @@ def test_fill_text_check(pdf_samples, static_pdfs, json_samples):
 
         assert len(expected) == len(actual)
         assert expected == actual
+
+
+@pytest.mark.web_api_test
+def test_fill_radio(pdf_samples, static_pdfs, json_samples):
+    expected_path = os.path.join(pdf_samples, "docs", "test_fill_radio.pdf")
+    path = os.path.join(static_pdfs, "sample_template_with_radio_button.pdf")
+    with (
+        open(path, "rb") as f,
+        open(os.path.join(json_samples, "test_fill_radio.json"), "r") as j,
+    ):
+        result = client.post(
+            "/fill",
+            data={"data": json.dumps(json.load(j))},
+            files={
+                "pdf": ("output.pdf", f, "application/pdf"),
+            },
+        )
+
+    with open(expected_path, "rb") as f:
+        expected = f.read()
+        actual = result.content
+
+        assert len(expected) == len(actual)
+        assert expected == actual
