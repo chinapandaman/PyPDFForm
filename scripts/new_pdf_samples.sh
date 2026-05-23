d796a964dd37e5c361cdbe1bc51805bc000986da
chinapandaman <benjaminli801@gmail.com>
2026-05-22 19:33:55 -0500

    PPF-1836: exclude cli tests from diff scripts

--- scripts/new_pdf_samples.sh
+++ scripts/new_pdf_samples.sh
@@ -2,4 +2,4 @@ if [ "$VIRTUAL_ENV" == "" ] && [ "$PYPDFFORM_ENV" != "container" ]; then
   source "./venv/bin/activate"
 fi
 
-PYTHONPATH=. pytest --regenerate=1
+PYTHONPATH=. pytest -m 'not cli_test' --regenerate=1
