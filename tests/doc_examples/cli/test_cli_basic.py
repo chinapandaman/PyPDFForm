ace3feba5ba038275c35be58d545410f18e3c7f9
chinapandaman <benjaminli801@gmail.com>
2026-07-05 11:01:55 -0500

    PPF-1989: this test is duplicated

--- tests/doc_examples/cli/test_cli_basic.py
+++ tests/doc_examples/cli/test_cli_basic.py
@@ -39,32 +39,6 @@ def test_json_file_input_for_fill(pdf_samples, static_pdfs, json_samples, tmp_pa
 
 
 @pytest.mark.cli_test
-def test_yaml_file_input_for_fill(pdf_samples, static_pdfs, yaml_samples, tmp_path):
-    expected_path = os.path.join(pdf_samples, "docs", "test_fill_text_check.pdf")
-    output_path = os.path.join(tmp_path, "output.pdf")
-
-    result = runner.invoke(
-        cli_app,
-        [
-            "fill",
-            os.path.join(static_pdfs, "sample_template.pdf"),
-            "-f",
-            os.path.join(yaml_samples, "test_fill_text_check.yaml"),
-            "-o",
-            output_path,
-        ],
-    )
-    assert result.exit_code == 0
-
-    with open(expected_path, "rb") as f1, open(output_path, "rb") as f2:
-        expected = f1.read()
-        actual = f2.read()
-
-        assert len(expected) == len(actual)
-        assert expected == actual
-
-
-@pytest.mark.cli_test
 def test_need_appearances_option(static_pdfs, tmp_path):
     output_path = os.path.join(tmp_path, "output.pdf")
     result = runner.invoke(
